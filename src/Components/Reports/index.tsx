e28539e4707ee417c224d027fe778f218bf67178
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-09-19 16:55:51 -0300

    Pagina de relatorios

---
+++ src/Components/Reports/index.tsx
@@ -0,0 +1,55 @@
+import { NavLink, Outlet } from 'react-router-dom';
+import {Container, Content,Sidebar, Header, SideContent,GraphContainer } from './styles';
+
+const Reports = () => {
+  return (
+    <Container>
+      <Content>
+        <Sidebar>
+          <Header>
+            <h3>Relatórios</h3>
+          </Header>
+          <SideContent>
+            <NavLink className='navlink' to='/reports/office'>
+              Cargos
+            </NavLink>
+            <NavLink className='navlink' to='/reports/addfield'>
+              Campos adicionais
+            </NavLink>
+            <NavLink className='navlink' to='/reports/new'>
+              Aditional Field
+            </NavLink>
+            <NavLink className='navlink' to='/reports/age'>
+              Faixa etária
+            </NavLink>
+            <NavLink className='navlink' to='/reports/birth'>
+              Aniversários
+            </NavLink>
+            <NavLink className='navlink' to='/reports/sex'>
+              Sexo
+            </NavLink>
+            <NavLink className='navlink' to='/reports/mstate'>
+              Estado civil
+            </NavLink>
+            <NavLink className='navlink' to='/reports/baptism'>
+              Batismo
+            </NavLink>
+            <NavLink className='navlink' to='/reports/conversion'>
+              Data de conversão
+            </NavLink>
+            <NavLink className='navlink' to='/reports/district'>
+              Bairro
+            </NavLink>
+            <NavLink className='navlink' to='/reports/permission'>
+              Permissões
+            </NavLink>
+          </SideContent>
+        </Sidebar>
+        <GraphContainer>
+          <Outlet />
+        </GraphContainer>
+      </Content>
+    </Container>
+  )
+}
+export default Reports;
---
+++ src/Components/Reports/styles.ts
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+`
+export const Content = styled.div`
+  width: 100%;
+  height: auto;
+  padding: 1rem;
+  display: flex;
+  align-items:start;
+  gap: 1rem;
+
+`
+export const Sidebar = styled.div`
+  width: 30%;
+  height: auto;
+  display: flex;
+  align-items:center;
+  flex-direction: column;
+  background: ${Props => Props.theme.colors.bac};
+  border-radius: 10px;
+`
+export const Header = styled.div`
+  width: 100%;
+  height: 50px;
+  border-bottom: 1px solid ${Props => Props.theme.colors.sec};
+  display: flex;
+  align-items:center;
+  padding: 0 1rem;
+`
+export const SideContent = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  align-items:center;
+  flex-direction: column;
+  padding: 1rem;
+  gap: .5rem;
+  .navlink{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-decoration: none;
+    color: ${Props =>Props.theme.colors.pri};
+    width: 100%;
+    height: 30px;
+    background: #f4f4f4;
+    border-radius: 5px;
+    font-size: 1rem;
+  }
+  .navlink.active {
+    background: ${Props => Props.theme.colors.sec};
+    color: #f4f4f4;
+  }
+`
+export const GraphContainer = styled.div`
+  width: 70%;
+  height: auto;
+  min-height: 300px;
+  display: flex;
+  align-items:center;
+  flex-direction: column;
+  background: ${Props => Props.theme.colors.bac};
+  border-radius: 10px;
+`
