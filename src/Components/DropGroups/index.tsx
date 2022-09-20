4391a8e10071c7efd45d014a592050c67776cb9a
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-09-20 18:27:02 -0300

    Menu drodown grupos

---
+++ src/Components/DropGroups/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Button from '../Button';
+import { Wrapper, Droped } from './styles'
+import { NavLink } from 'react-router-dom';
+
+const DropGroups = () => {
+  const [dropedOpen, setDropedOpen] = React.useState(false);
+
+  return (
+    <Wrapper>
+      <Button
+        onMouseEnter={() => setDropedOpen(true)}
+        onMouseLeave={() => setDropedOpen(false)}
+      >Grupos</Button>
+      {dropedOpen ?
+        <>
+          <Droped onMouseEnter={() => setDropedOpen(true)}
+            onMouseLeave={() => setDropedOpen(false)} className='droped'>
+            <NavLink className='navlink' to='/groups'>
+              <Button>Grupos</Button>
+            </NavLink>
+            <NavLink className='navlink' to='/addgroups'>
+              <Button>Adicionar grupo</Button>
+            </NavLink>
+            <NavLink className='navlink' to='/groupcategories'>
+              <Button>Categorias de grupos</Button>
+            </NavLink>
+            <NavLink className='navlink' to='/report'>
+              <Button>Relatorios</Button>
+            </NavLink>
+            <NavLink className='navlink' to='/importexport'>
+              <Button>Importat/Exportar</Button>
+            </NavLink>
+          </Droped>
+        </> : null}
+    </Wrapper>
+  )
+}
+
+export default DropGroups;
