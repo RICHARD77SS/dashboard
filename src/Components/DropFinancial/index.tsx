e9d7e705773c435a0efe5d2e07b0bff21a1a50c0
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-09-23 18:22:26 -0300

    Menu dropdown financias

---
+++ src/Components/DropFinancial/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Button from '../Button';
+import { Wrapper, Droped } from './styles'
+import { NavLink } from 'react-router-dom';
+
+const DropFinancial = () => {
+  const [dropedOpen, setDropedOpen] = React.useState(false);
+
+  return (
+    <Wrapper>
+      <Button
+        onMouseEnter={() => setDropedOpen(true)}
+        onMouseLeave={() => setDropedOpen(false)}
+      >Financias</Button>
+      {dropedOpen ?
+        <>
+          <Droped onMouseEnter={() => setDropedOpen(true)}
+            onMouseLeave={() => setDropedOpen(false)} className='droped'>
+            <NavLink className='navlink' to='/financial'>
+              <Button>Financias</Button>
+            </NavLink>
+            <NavLink className='navlink' to='/financialtransactions'>
+              <Button>Transações</Button>
+            </NavLink>
+            <NavLink className='navlink' to='/financialreports'>
+              <Button>Relatorios</Button>
+            </NavLink>
+            <NavLink className='navlink' to='/financialcategories'>
+              <Button>Categorias</Button>
+            </NavLink>
+            <NavLink className='navlink' to='/financialacconts'>
+              <Button>Contas</Button>
+            </NavLink>
+            <NavLink className='navlink' to='/financialcontacts'>
+              <Button>Contatos</Button>
+            </NavLink>
+            <NavLink className='navlink' to='/financialcustcenter'>
+              <Button>Centros de custos</Button>
+            </NavLink>
+            <NavLink className='navlink' to='/financialimportsexports'>
+              <Button>Importar/exportar</Button>
+            </NavLink>
+          </Droped>
+        </> : null}
+    </Wrapper>
+  )
+}
+
+export default DropFinancial;
---
+++ src/Components/DropFinancial/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+
+export const Wrapper = styled.div`
+
+
+`
+export const Droped = styled.div`
+  width:250px;
+  height: auto;
+  background: ${Props => Props.theme.colors.sec};
+  padding: 1rem;
+  position: absolute;
+  top: 50px;
+  border-radius: 10px;
+`
