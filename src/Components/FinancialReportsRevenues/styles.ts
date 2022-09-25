82e3627d47372a4421291881e4354ff1c114390a
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-09-25 20:21:46 -0300

    Receitas e despesas

---
+++ src/Components/FinancialReportsRevenues/styles.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+export const Hr = styled.hr`
+  width: 100%;
+  height: 1px;
+`
+export const ResumeBox = styled.div`
+  width: 100%;
+  height: auto;
+  border: 1px solid black;
+`
+export const ResumeFlex = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+
+`
+export const ResumeBlock = styled.div`
+  width: 50%;
+  height: auto;
+
+  display: flex;
+  align-items:center;
+  flex-direction: column;
+`
+export const ResumeContent = styled.div`
+  width: 100%;
+  height: auto;
+  border: 1px solid black;
+  background: #f4f4f4;
+  display: flex;
+  align-items:center;
+  justify-content:center;
+  flex-direction: column;
+  pre {
+    padding: 0 1rem;
+  }
+`
