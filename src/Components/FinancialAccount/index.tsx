b9d49efc7b1a421e4d6278a757157ef9b6f22448
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-09-26 18:11:51 -0300

    Contas

---
+++ src/Components/FinancialAccount/index.tsx
@@ -0,0 +1,70 @@
+import Box from "../Box";
+import BoxContent from "../BoxContent";
+import BoxHeader from "../BoxHeader";
+import Button from "../Button";
+import Container from "../Container";
+import Content from "../Content";
+import EditRemove from "../EditRemove";
+import Flex from "../Flex";
+import Input from "../Input";
+import InputBlock from "../InputBlock";
+import Table from "../Table";
+import Tbody from "../Tbody";
+import Td from "../Td";
+import Th from "../Th";
+import Thead from "../Thead";
+import Tr from "../Tr";
+
+import { Aside, AsideHeader, AsideContent } from './styles';
+
+const FinancialAccount = () => {
+  return (
+    <Container>
+      <h3>Contas</h3>
+      <Content>
+        <Box>
+          <BoxHeader title="Resultados: 2" />
+          <BoxContent>
+            <p>Cadastre suas contas bancárias ou caixas.</p>
+            <Content>
+              <Table>
+                <Thead>
+                  <Tr>
+                    <Th>Nome da categoria</Th>
+                    <Th>Descrição</Th>
+                    <Th>Ações</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td>Name categorie</Td>
+                    <Td>Descrição</Td>
+                    <Td><EditRemove /></Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </Content>
+          </BoxContent>
+        </Box>
+        <Aside>
+          <AsideHeader>
+            <h3>+ Criar conta</h3>
+          </AsideHeader>
+          <AsideContent>
+            <InputBlock>
+              <label htmlFor="name">Nomde da conta</label>
+              <Input type='text' />
+            </InputBlock>
+            <InputBlock>
+              <label htmlFor="description">Descrição</label>
+              <textarea title='area' name="" id="" ></textarea>
+            </InputBlock>
+            <Button>Criar</Button>
+          </AsideContent>
+        </Aside>
+      </Content>
+    </Container>
+  )
+}
+
+export default FinancialAccount;
