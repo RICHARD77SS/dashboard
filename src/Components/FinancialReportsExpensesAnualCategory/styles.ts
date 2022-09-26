efca64d1d8ce459e0b1c81e913d78911a1aff4c8
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-09-26 18:15:20 -0300

    Despesas anuais

---
+++ src/Components/FinancialReportsExpensesAnualCategory/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Filters = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-wrap: wrap;
+  align-items:center;
+  justify-content: center;
+  gap: .5rem;
+  padding: 1rem;
+`
