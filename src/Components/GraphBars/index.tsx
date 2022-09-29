9735a55b3219a3785e96619c2788234ccf64ac88
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-09-29 17:57:28 -0300

    Grafico de barras

---
+++ src/Components/GraphBars/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+};
+
+interface Props {
+  data: any;
+}
+const GraphBars =(Props:Props)=> {
+  return <Bar options={options} data={Props.data} />;
+}
+
+export default GraphBars;
