da2be841b5bc57dd7f8824e26f78abe19593b8d7
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-10-03 18:14:10 -0300

    Funcionalidade para inserir background e adicionar arquivo de imagem

--- src/Components/CardMake/index.tsx
+++ src/Components/CardMake/index.tsx
@@ -1,20 +1,43 @@
-
+import React from 'react';
 import Block from "../Block"
 import Box from "../Box"
 import BoxContent from "../BoxContent"
 import BoxHeader from "../BoxHeader"
 import Button from "../Button"
+import CardFront from '../CardFront';
 import Container from "../Container"
 import Content from "../Content"
 import Flex from "../Flex"
 import Input from "../Input"
-import InputBlock from "../InputBlock"
 
-import { BoxFlex, Side, Make, CardContent, CardComplete, Backgrounds, Bg } from './styles'
+import { BoxFlex, Side, Make, CardContent, BgButton, Backgrounds, Bg } from './styles'
 
 const CardMake = () => {
+
+  const [bgFront, setBgFront] = React.useState('');
+  const [bgBack, setBgBack] = React.useState('');
+
+
+  const [selectedImage, setSelectedImage] = React.useState<File>();
+  const [selectedImageBack, setSelectedImageBack] = React.useState<File>();
+
+  if (selectedImage) {
+    var url = URL.createObjectURL(selectedImage)
+  } else {
+    var url = ''
+  }
+  if (selectedImageBack) {
+    var url2 = URL.createObjectURL(selectedImageBack)
+  } else {
+    var url2 = ''
+  }
+  console.log(url)
   return (
     <Container>
+      <br />
+      <br />
+      <br />
+      <br />
       <Content>
         <Block>
           <Box>
@@ -47,39 +70,42 @@ const CardMake = () => {
                 </Side>
                 <Make>
                   <CardContent>
-                    <CardComplete>
-                      <img src="" alt="" />
-                      <InputBlock>
-                        <img src="" alt="" />
-                        <p>12384</p>
-                      </InputBlock>
-                      <h3>title</h3>
-                      <span>Local</span>
-                      <h4>Cartão de membro</h4>
-                      <InputBlock>
-                        <h5>Nome completo</h5>
-                        <p>Nome here</p>
-                      </InputBlock>
-                      <InputBlock>
-                        <h5>Cargos</h5>
-                        <p>Diretor</p>
-                      </InputBlock>
-                      <InputBlock>
-                        <h5>Estado civil</h5>
-                        <p>Casado(a)</p>
-                      </InputBlock>
-                      <InputBlock>
-                        <h5>Data de nascimento</h5>
-                        <p>30/08/2000</p>
-                      </InputBlock>
-                    </CardComplete>
+                    <CardFront cardBg={bgFront} />
+
                     <Backgrounds>
-                      <Bg>
-                        <img src="" alt="" />
-                      </Bg>
-                      <Bg>
-                        <img src="" alt="" />
-                      </Bg>
+                      {selectedImage && (
+                        <BgButton onClick={() => { setBgFront(`${url}`) }}>
+                          <Bg>
+                            <img alt="not fount" width={"250px"} src={URL.createObjectURL(selectedImage)} />
+                            <button onClick={() => setSelectedImage(undefined)}>Remove</button>
+                          </Bg>
+                        </BgButton>
+                      )}
+                      <BgButton onClick={() => { setBgFront('') }}>
+                        <Bg>
+                          <h5>Fazer upload<br /> de imagem</h5>
+                          <Input type="file"
+                            name="myImage"
+                            onChange={(event) => {
+                              setSelectedImage(event.target.files![0]);
+                            }} />
+                        </Bg>
+                      </BgButton>
+                      <BgButton onClick={() => { setBgFront("https://img.freepik.com/fotos-gratis/jovem-viajante-caminhando-sozinho-com-espaco-de-copia_23-2149119538.jpg?w=740&t=st=1664830923~exp=1664831523~hmac=562f24a6e30554a5e66b1418d3b2143267d707d8ef6c6dd3337275f055a39172") }}>
+                        <Bg>
+                          <img src="https://img.freepik.com/fotos-gratis/jovem-viajante-caminhando-sozinho-com-espaco-de-copia_23-2149119538.jpg?w=740&t=st=1664830923~exp=1664831523~hmac=562f24a6e30554a5e66b1418d3b2143267d707d8ef6c6dd3337275f055a39172" alt="" />
+                        </Bg>
+                      </BgButton>
+                      <BgButton onClick={() => { setBgFront("https://img.freepik.com/fotos-gratis/foto-hipnotizante-de-uma-colina-rochosa-verde-durante-a-bela-hora-do-por-do-sol_181624-30359.jpg?w=740&t=st=1664826956~exp=1664827556~hmac=379cae0afc720e8cc279890186249068cddfc26bc2896b5451dc66ea8943892e") }}>
+                        <Bg>
+                          <img src="https://img.freepik.com/fotos-gratis/foto-hipnotizante-de-uma-colina-rochosa-verde-durante-a-bela-hora-do-por-do-sol_181624-30359.jpg?w=740&t=st=1664826956~exp=1664827556~hmac=379cae0afc720e8cc279890186249068cddfc26bc2896b5451dc66ea8943892e" alt="" />
+                        </Bg>
+                      </BgButton>
+                      <BgButton onClick={() => { setBgFront("https://img.freepik.com/fotos-gratis/jovem-viajante-curtindo-ambiente-rural_23-2149125547.jpg?w=740&t=st=1664826984~exp=1664827584~hmac=727c5a1370210628462a9c4fe51c9c3c5a13b05d437d124f4bce227db53036d3") }}>
+                        <Bg>
+                          <img src="https://img.freepik.com/fotos-gratis/jovem-viajante-curtindo-ambiente-rural_23-2149125547.jpg?w=740&t=st=1664826984~exp=1664827584~hmac=727c5a1370210628462a9c4fe51c9c3c5a13b05d437d124f4bce227db53036d3" alt="" />
+                        </Bg>
+                      </BgButton>
                     </Backgrounds>
                   </CardContent>
                 </Make>
@@ -111,39 +137,41 @@ const CardMake = () => {
                 </Side>
                 <Make>
                   <CardContent>
-                    <CardComplete>
-                      <img src="" alt="" />
-                      <InputBlock>
-                        <img src="" alt="" />
-                        <p>12384</p>
-                      </InputBlock>
-                      <h3>title</h3>
-                      <span>Local</span>
-                      <h4>Cartão de membro</h4>
-                      <InputBlock>
-                        <h5>Nome completo</h5>
-                        <p>Nome here</p>
-                      </InputBlock>
-                      <InputBlock>
-                        <h5>Cargos</h5>
-                        <p>Diretor</p>
-                      </InputBlock>
-                      <InputBlock>
-                        <h5>Estado civil</h5>
-                        <p>Casado(a)</p>
-                      </InputBlock>
-                      <InputBlock>
-                        <h5>Data de nascimento</h5>
-                        <p>30/08/2000</p>
-                      </InputBlock>
-                    </CardComplete>
+                    <CardFront cardBg={bgBack} />
                     <Backgrounds>
-                      <Bg>
-                        <img src="" alt="" />
-                      </Bg>
-                      <Bg>
-                        <img src="" alt="" />
-                      </Bg>
+                      {selectedImageBack && (
+                        <BgButton onClick={() => { setBgBack(`${url2}`) }}>
+                          <Bg>
+                            <img alt="not fount" width={"250px"} src={URL.createObjectURL(selectedImageBack)} />
+                            <button onClick={() => setSelectedImageBack(undefined)}>Remove</button>
+                          </Bg>
+                        </BgButton>
+                      )}
+                      <BgButton onClick={() => { setBgBack('') }}>
+                        <Bg>
+                          <h5>Fazer upload<br /> de imagem</h5>
+                          <Input type="file"
+                            name="myImage"
+                            onChange={(event) => {
+                              setSelectedImageBack(event.target.files![0]);
+                            }} />
+                        </Bg>
+                      </BgButton>
+                      <BgButton onClick={() => { setBgBack("https://img.freepik.com/fotos-gratis/retrato-de-um-lindo-cao-pastor-australiano-domestico-marrom-e-branco-posando-na-natureza-ao-por-do-sol_181624-59364.jpg?w=740&t=st=1664825293~exp=1664825893~hmac=f9c0003db67e0a60bc7093ec9add1752891382f84d78b527ac6d7ea18759f579") }}>
+                        <Bg>
+                          <img src="https://img.freepik.com/fotos-gratis/retrato-de-um-lindo-cao-pastor-australiano-domestico-marrom-e-branco-posando-na-natureza-ao-por-do-sol_181624-59364.jpg?w=740&t=st=1664825293~exp=1664825893~hmac=f9c0003db67e0a60bc7093ec9add1752891382f84d78b527ac6d7ea18759f579" alt="" />
+                        </Bg>
+                      </BgButton>
+                      <BgButton onClick={() => { setBgBack("https://img.freepik.com/fotos-gratis/foto-hipnotizante-de-uma-colina-rochosa-verde-durante-a-bela-hora-do-por-do-sol_181624-30359.jpg?w=740&t=st=1664826956~exp=1664827556~hmac=379cae0afc720e8cc279890186249068cddfc26bc2896b5451dc66ea8943892e") }}>
+                        <Bg>
+                          <img src="https://img.freepik.com/fotos-gratis/foto-hipnotizante-de-uma-colina-rochosa-verde-durante-a-bela-hora-do-por-do-sol_181624-30359.jpg?w=740&t=st=1664826956~exp=1664827556~hmac=379cae0afc720e8cc279890186249068cddfc26bc2896b5451dc66ea8943892e" alt="" />
+                        </Bg>
+                      </BgButton>
+                      <BgButton onClick={() => { setBgBack("https://img.freepik.com/fotos-gratis/jovem-viajante-curtindo-ambiente-rural_23-2149125547.jpg?w=740&t=st=1664826984~exp=1664827584~hmac=727c5a1370210628462a9c4fe51c9c3c5a13b05d437d124f4bce227db53036d3") }}>
+                        <Bg>
+                          <img src="https://img.freepik.com/fotos-gratis/jovem-viajante-curtindo-ambiente-rural_23-2149125547.jpg?w=740&t=st=1664826984~exp=1664827584~hmac=727c5a1370210628462a9c4fe51c9c3c5a13b05d437d124f4bce227db53036d3" alt="" />
+                        </Bg>
+                      </BgButton>
                     </Backgrounds>
                   </CardContent>
                 </Make>
@@ -152,6 +180,10 @@ const CardMake = () => {
           </Box>
         </Block>
       </Content>
+      <br />
+      <br />
+      <br />
+      <br />
     </Container>
   )
 }
