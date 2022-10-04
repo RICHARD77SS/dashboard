4c4bf20006615af8a73473b064517b28bd524023
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-10-04 13:40:47 -0300

    Modal para adicionar textos ao mural de avisos

---
+++ src/Components/MuralsAddText/index.tsx
@@ -0,0 +1,97 @@
+import Block from "../Block";
+import Button from "../Button";
+import Flex from "../Flex";
+import FlexBetween from "../FlexBetween";
+import Input from "../Input";
+import InputBlock from "../InputBlock";
+
+import { Container, Content, Box, BoxHeader, BoxContent, BoxFlex, BoxBLock, Bg, BoxFooter, Closed } from './styles'
+
+interface Props {
+  children?: React.ReactNode;
+  onClick?: any;
+}
+const MuralsAddText = ({ children, onClick }: Props) => {
+  return (
+    <Container >
+      <Content>
+        <Box>
+          <BoxHeader>
+            <FlexBetween>
+              <h3>Adicionar</h3>
+              {children}
+            </FlexBetween>
+          </BoxHeader>
+          <BoxContent>
+            <Input placeholder='título' type='text' />
+            <textarea placeholder='Descrição' title='text' name="" id="" ></textarea>
+            <FlexBetween>
+              <Flex>
+                <p>Cor do texto</p>
+                <Input type='color' />
+              </Flex>
+              <Flex>
+                <p>Cor do fundo</p>
+                <Input type='color' />
+              </Flex>
+            </FlexBetween>
+            <Block>
+              <p>Extilo do fundo</p>
+              <BoxFlex>
+                <BoxBLock>
+                  <Bg>Texto</Bg>
+                  <Input type='checkbox' />
+                </BoxBLock>
+                <BoxBLock>
+                  <Bg>Texto</Bg>
+                  <Input type='checkbox' />
+                </BoxBLock>
+                <BoxBLock>
+                  <Bg>Texto</Bg>
+                  <Input type='checkbox' />
+                </BoxBLock>
+              </BoxFlex>
+            </Block>
+            <FlexBetween>
+              <InputBlock>
+                <p>Status</p>
+                <Flex>
+                  <Input id='publi' type='radio' />
+                  <label htmlFor="publi">Publicado</label>
+                </Flex>
+                <Flex>
+                  <Input id='despubli' type='radio' />
+                  <label htmlFor="despubli">Despublicado</label>
+                </Flex>
+              </InputBlock>
+              <InputBlock>
+                <p>Despublicar automaticamente</p>
+                <Flex>
+                  <Input id='never' type='radio' />
+                  <label htmlFor="never">Nunca</label>
+                </Flex>
+                <Flex>
+                  <Input id='postdate' type='radio' />
+                  <label htmlFor="postdate">Após esta data</label>
+                </Flex>
+              </InputBlock>
+            </FlexBetween>
+            <InputBlock>
+              <label htmlFor="link">Link externo</label>
+              <Input type='text' />
+            </InputBlock>
+            <Flex>
+              <Input type='checkbox' />
+              <label htmlFor="notification">Enviar notificação push</label>
+            </Flex>
+          </BoxContent>
+          <BoxFooter>
+            <Button>Salvar</Button>
+          </BoxFooter>
+        </Box>
+      </Content>
+      <Closed onClick={onClick}></Closed>
+    </Container>
+  )
+}
+export default MuralsAddText;
