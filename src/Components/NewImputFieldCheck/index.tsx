f414399f0c3581b0653e03cada498e39f7d9198a
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-10-05 15:33:25 -0300

    Modal para adicionar campos de imput

---
+++ src/Components/NewImputFieldCheck/index.tsx
@@ -0,0 +1,47 @@
+import Button from "../Button";
+import FlexBetween from "../FlexBetween";
+import Input from "../Input";
+import InputBlock from "../InputBlock";
+import { Container, Content, Box, BoxHeader, BoxContent, BoxFooter, Closed } from './styles'
+
+
+interface Props {
+  children?: React.ReactNode;
+  onClick?: any;
+}
+const NewImputFieldCheck = ({ children, onClick }: Props) => {
+
+  return (
+    <Container>
+      <Content>
+        <Box>
+          <BoxHeader>
+            <FlexBetween>
+              <h3>Criar</h3>
+              {children}
+            </FlexBetween>
+          </BoxHeader>
+          <BoxContent>
+            <InputBlock>
+              <label htmlFor="name">Nome do campo</label>
+              <Input id='name' placeholder='Nome do campo' type='text' />
+            </InputBlock>
+            <InputBlock>
+              <label htmlFor=" ">Opções</label>
+              <Input type='text' placeholder='Escreva aqui (o campo vazio será removido)' />
+              <Input type='text' placeholder='Escreva aqui (o campo vazio será removido)' />
+              <Input type='text' placeholder='Escreva aqui (o campo vazio será removido)' />
+            </InputBlock>
+            <Button>+ Adicionar nova opção</Button>
+          </BoxContent>
+          <BoxFooter>
+            <Button>Salvar</Button>
+          </BoxFooter>
+        </Box>
+      </Content>
+      <Closed onClick={onClick}></Closed>
+    </Container>
+  )
+}
+
+export default NewImputFieldCheck;
