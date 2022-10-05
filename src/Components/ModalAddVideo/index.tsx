f85fcaea4f361cd3ca6db055836660d4e0c184d4
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-10-05 15:32:20 -0300

    Modal para adiçionar videos

---
+++ src/Components/ModalAddVideo/index.tsx
@@ -0,0 +1,44 @@
+
+import Button from "../Button";
+
+import FlexBetween from "../FlexBetween";
+import Input from "../Input";
+import InputBlock from "../InputBlock";
+
+import { Container, Content, Box, BoxHeader, BoxContent, BoxFooter, Closed } from '../NewImputFieldCheck/styles'
+
+interface Props {
+  children?: React.ReactNode;
+  onClick?: any;
+}
+const ModalAddVideo = ({ children, onClick }: Props) => {
+  return (
+    <Container >
+      <Content>
+        <Box>
+          <BoxHeader>
+            <FlexBetween>
+              <h3>Adicionar vídeo</h3>
+              {children}
+            </FlexBetween>
+          </BoxHeader>
+          <BoxContent>
+            <InputBlock>
+              <label htmlFor="title">Título</label>
+              <Input placeholder='título' type='text' />
+            </InputBlock>
+            <InputBlock>
+              <label htmlFor="link">Link/URL do Vídeo (Youtube)</label>
+              <Input id="link" type='text' placeholder='Video link'/>
+            </InputBlock>
+          </BoxContent>
+          <BoxFooter>
+            <Button>Salvar</Button>
+          </BoxFooter>
+        </Box>
+      </Content>
+      <Closed onClick={onClick}></Closed>
+    </Container>
+  )
+}
+export default ModalAddVideo;
