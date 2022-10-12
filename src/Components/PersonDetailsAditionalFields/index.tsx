a24205ef582eda2f2c8736ffeeed810e8fe9c330
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-10-12 18:41:49 -0300

    campos adicionais

--- src/Components/PersonDetailsAditionalFields/index.tsx
+++ src/Components/PersonDetailsAditionalFields/index.tsx
@@ -1,31 +1,47 @@
 
+import { useAxios } from "../../hooks/useAxios";
 import BoxHeader from "../BoxHeader";
+import Flex from "../Flex";
+import Input from "../Input";
 
 import { Container, Content, Block, Box, BoxContent } from './styles'
 interface Props {
-  
+
 }
 const PersonDetailsAditionalFields = (Props: Props) => {
+  const { data } = useAxios('extraFields')
+  console.log(data)
   return (
     <Container >
       <Content>
         <Block>
-          <Box>
-            <BoxHeader title='Dados pessoais'></BoxHeader>
-            <BoxContent>
-              
-            </BoxContent>
-          </Box>
+          {data?.extraFields?.map((fields: any) => {
+            return (
+              <Box>
+                <BoxHeader title={fields.inputName}></BoxHeader>
+                <BoxContent>
+                  {fields.inputOption.map((input: any) => {
+                    return (
+                      <Flex>
+                        <label htmlFor="">{input}</label>
+                        <Input type={fields.inputType} />
+                      </Flex>
+                    )
+                  })}
+                </BoxContent>
+              </Box>
+            )
+          })}
         </Block>
 
         <Block>
           <Box>
             <BoxHeader title='Contatos'></BoxHeader>
             <BoxContent>
-              
+
             </BoxContent>
           </Box>
-    
+
         </Block>
       </Content>
     </Container>
