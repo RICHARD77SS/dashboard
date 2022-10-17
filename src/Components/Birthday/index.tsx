04e0c3aac4a9cf034555b30447ed7534d4f1f03f
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-10-17 19:31:43 -0300

    Aniversariantes de cada mes

--- src/Components/Birthday/index.tsx
+++ src/Components/Birthday/index.tsx
@@ -1,12 +1,29 @@
 import Button from '../Button';
 import Input from '../Input';
+import { useAxios } from '../../hooks/useAxios';
 
-import { Container, ContainerHeader, Content, Header, BoxContent, Top, Center, Options, Table, Thead, Tr, Th, Tbody, Td, Bottom } from './styles';
+import { Container, ContainerHeader, Content, Header, BoxContent, Top, Center, Options, Bottom } from './styles';
 
 import { AiOutlineCopy, AiOutlineDownload, AiOutlinePrinter, AiOutlineEye } from 'react-icons/ai';
 import { BsArrowDown, BsArrowUp } from 'react-icons/bs';
+import Table from '../Table';
+import Thead from '../Thead';
+import Tr from '../Tr';
+import Th from '../Th';
+import Tbody from '../Tbody';
+import Td from '../Td';
+import TabsBirthday from '../TabsBirthday/index';
+import Tab from '../Tab';
+
 
 const Birthday = () => {
+  const { data } = useAxios('person')
+  const jan = document.getElementById('jan')?.parentNode
+
+  const ago = document.getElementById('ago')
+
+
+  console.log(!ago)
   return (
     <Container>
       <ContainerHeader>
@@ -18,20 +35,358 @@ const Birthday = () => {
           <h3>Selecione um mês</h3>
         </Header>
         <BoxContent>
-          <Top>
-            <Button>jan(0)</Button>
-            <Button>fev(0)</Button>
-            <Button>mar(0)</Button>
-            <Button>abr(0)</Button>
-            <Button>mai(0)</Button>
-            <Button>jun(0)</Button>
-            <Button>jul(0)</Button>
-            <Button>ago(0)</Button>
-            <Button>set(0)</Button>
-            <Button>out(0)</Button>
-            <Button>nov(0)</Button>
-            <Button>dez(0)</Button>
-          </Top>
+          <TabsBirthday>
+            <Tab title={`Jan ${(0)}`}>
+              <Table>
+                <Thead>
+                  <Tr>
+                    <Th><Button>Dia <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Imagem <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Nome <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                  </Tr>
+                </Thead>
+                <Tbody id='jan'>
+
+                  {data?.person.map((person: any, index: any) => {
+                    const data = new Date(person.birth)
+                    const month = data.getMonth() + 1
+                    if (month === 1) {
+
+                      return (
+                        <Tr key={index}>
+                          <Td>{data.toLocaleDateString()}</Td>
+                          <Td>Image here</Td>
+                          <Td>{person.name}</Td>
+                        </Tr>
+                      )
+                    }
+
+                    return (<></>)
+                  })}
+                </Tbody>
+              </Table>
+            </Tab>
+            <Tab title={`Fev ${(0)}`}>
+              <Table>
+                <Thead>
+                  <Tr>
+                    <Th><Button>Dia <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Imagem <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Nome <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data?.person.map((person: any, index: any) => {
+                    const data = new Date(person.birth)
+                    const month = data.getMonth() + 1
+                    if (month === 2) {
+
+                      return (
+                        <Tr key={index}>
+                          <Td>{data.toLocaleDateString()}</Td>
+                          <Td>Image here</Td>
+                          <Td>{person.name}</Td>
+                        </Tr>
+                      )
+                    }
+
+                    return (<></>)
+                  })}
+                </Tbody>
+              </Table>
+            </Tab>
+            <Tab title={`Mar ${(0)}`}>
+              <Table>
+                <Thead>
+                  <Tr>
+                    <Th><Button>Dia <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Imagem <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Nome <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data?.person.map((person: any, index: any) => {
+                    const data = new Date(person.birth)
+                    const month = data.getMonth() + 1
+                    if (month === 3) {
+
+                      return (
+                        <Tr key={index}>
+                          <Td>{data.toLocaleDateString()}</Td>
+                          <Td>Image here</Td>
+                          <Td>{person.name}</Td>
+                        </Tr>
+                      )
+                    }
+
+                    return (<></>)
+                  })}
+                </Tbody>
+              </Table>
+            </Tab>
+            <Tab title={`Abr ${(0)}`}>
+              <Table>
+                <Thead>
+                  <Tr>
+                    <Th><Button>Dia <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Imagem <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Nome <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data?.person.map((person: any, index: any) => {
+                    const data = new Date(person.birth)
+                    const month = data.getMonth() + 1
+                    if (month === 4) {
+
+                      return (
+                        <Tr key={index}>
+                          <Td>{data.toLocaleDateString()}</Td>
+                          <Td>Image here</Td>
+                          <Td>{person.name}</Td>
+                        </Tr>
+                      )
+                    }
+
+                    return (<></>)
+                  })}
+                </Tbody>
+              </Table>
+            </Tab>
+            <Tab title={`Mai ${(0)}`}>
+              <Table>
+                <Thead>
+                  <Tr>
+                    <Th><Button>Dia <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Imagem <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Nome <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data?.person.map((person: any, index: any) => {
+                    const data = new Date(person.birth)
+                    const month = data.getMonth() + 1
+                    if (month === 5) {
+
+                      return (
+                        <Tr key={index}>
+                          <Td>{data.toLocaleDateString()}</Td>
+                          <Td>Image here</Td>
+                          <Td>{person.name}</Td>
+                        </Tr>
+                      )
+                    }
+
+                    return (<></>)
+                  })}
+                </Tbody>
+              </Table>
+            </Tab>
+            <Tab title={`Jun ${(0)}`}>
+              <Table>
+                <Thead>
+                  <Tr>
+                    <Th><Button>Dia <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Imagem <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Nome <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data?.person.map((person: any, index: any) => {
+                    const data = new Date(person.birth)
+                    const month = data.getMonth() + 1
+                    if (month === 6) {
+
+                      return (
+                        <Tr key={index}>
+                          <Td>{data.toLocaleDateString()}</Td>
+                          <Td>Image here</Td>
+                          <Td>{person.name}</Td>
+                        </Tr>
+                      )
+                    }
+
+                    return (<></>)
+                  })}
+                </Tbody>
+              </Table>
+            </Tab>
+            <Tab title={`Jul ${(0)}`}>
+              <Table>
+                <Thead>
+                  <Tr>
+                    <Th><Button>Dia <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Imagem <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Nome <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data?.person.map((person: any, index: any) => {
+                    const data = new Date(person.birth)
+                    const month = data.getMonth() + 1
+                    if (month === 7) {
+
+                      return (
+                        <Tr key={index}>
+                          <Td>{data.toLocaleDateString()}</Td>
+                          <Td>Image here</Td>
+                          <Td>{person.name}</Td>
+                        </Tr>
+                      )
+                    }
+
+                    return (<></>)
+                  })}
+                </Tbody>
+              </Table>
+            </Tab>
+            <Tab title={`Ago ${(0)}`}>
+              <Table>
+                <Thead>
+                  <Tr>
+                    <Th><Button>Dia <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Imagem <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Nome <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                  </Tr>
+                </Thead>
+                <Tbody id='ago'>
+                  {data?.person.map((person: any, index: any) => {
+                    const data = new Date(person.birth)
+                    const month = data.getMonth() + 1
+                    if (month === 8) {
+
+                      return (
+                        <Tr key={index}>
+                          <Td>{data.toLocaleDateString()}</Td>
+                          <Td>Image here</Td>
+                          <Td>{person.name}</Td>
+                        </Tr>
+                      )
+                    }
+
+                    return (<></>)
+                  })}
+
+                </Tbody>
+              </Table>
+            </Tab>
+            <Tab title={`Set ${(0)}`}>
+              <Table>
+                <Thead>
+                  <Tr>
+                    <Th><Button>Dia <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Imagem <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Nome <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data?.person.map((person: any, index: any) => {
+                    const data = new Date(person.birth)
+                    const month = data.getMonth() + 1
+                    if (month === 9) {
+
+                      return (
+                        <Tr key={index}>
+                          <Td>{data.toLocaleDateString()}</Td>
+                          <Td>Image here</Td>
+                          <Td>{person.name}</Td>
+                        </Tr>
+                      )
+                    }
+
+                    return (<></>)
+                  })}
+                </Tbody>
+              </Table>
+            </Tab>
+            <Tab title={`Out ${(0)}`}>
+              <Table>
+                <Thead>
+                  <Tr>
+                    <Th><Button>Dia <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Imagem <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Nome <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data?.person.map((person: any, index: any) => {
+                    const data = new Date(person.birth)
+                    const month = data.getMonth() + 1
+                    if (month === 10) {
+
+                      return (
+                        <Tr key={index}>
+                          <Td>{data.toLocaleDateString()}</Td>
+                          <Td>Image here</Td>
+                          <Td>{person.name}</Td>
+                        </Tr>
+                      )
+                    }
+
+                    return (<></>)
+                  })}
+                </Tbody>
+              </Table>
+            </Tab>
+            <Tab title={`Nov ${(0)}`}>
+              <Table>
+                <Thead>
+                  <Tr>
+                    <Th><Button>Dia <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Imagem <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Nome <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data?.person.map((person: any, index: any) => {
+                    const data = new Date(person.birth)
+                    const month = data.getMonth() + 1
+                    if (month === 11) {
+
+                      return (
+                        <Tr key={index}>
+                          <Td>{data.toLocaleDateString()}</Td>
+                          <Td>Image here</Td>
+                          <Td>{person.name}</Td>
+                        </Tr>
+                      )
+                    }
+
+                    return (<></>)
+                  })}
+                </Tbody>
+              </Table>
+            </Tab>
+            <Tab title={`Dez ${(0)}`}>
+              <Table>
+                <Thead>
+                  <Tr>
+                    <Th><Button>Dia <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Imagem <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                    <Th><Button>Nome <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data?.person.map((person: any, index: any) => {
+                    const data = new Date(person.birth)
+                    const month = data.getMonth() + 1
+                    if (month === 12) {
+
+                      return (
+                        <Tr key={index}>
+                          <Td>{data.toLocaleDateString()}</Td>
+                          <Td>Image here</Td>
+                          <Td>{person.name}</Td>
+                        </Tr>
+                      )
+                    }
+
+                    return (<></>)
+                  })}
+                </Tbody>
+              </Table>
+            </Tab>
+          </TabsBirthday>
           <Center>
             <Options>
               <Input type='number' step='20' min='0' max='100' />
@@ -44,22 +399,7 @@ const Birthday = () => {
               <Button><AiOutlineEye />Colunas</Button>
             </Options>
           </Center>
-          <Table>
-            <Thead>
-              <Tr>
-                <Th><Button>Dia <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
-                <Th><Button>Imagem <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
-                <Th><Button>Nome <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td>Data here</Td>
-                <Td>Image here</Td>
-                <Td>Name here</Td>
-              </Tr>
-            </Tbody>
-          </Table>
+
           <Bottom>
             <Button>Anterior</Button>
             <Button>Proximo</Button>
