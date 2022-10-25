d46b3fff2d503d418e31c40374fb741b7d2cf71f
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-10-25 20:35:35 -0300

    Adição dos dados no grafico de faixa etária

--- src/Components/OverviewPersons/index.tsx
+++ src/Components/OverviewPersons/index.tsx
@@ -6,11 +6,11 @@ import Button from "../Button";
 import Input from "../Input";
 import GraphLineArea from "../GraphLineArea"
 
-import { Graph, Graph1, Grid1, GraphContainer, Datalist, Div, User, Image } from './styles'
-import Content from "../Content";
+import { Content, Graph, Graph1, Grid1, GraphContainer, Datalist, Div, User, Image } from './styles'
 import Block from "../Block";
 import GraphPieArea from "../GraphPieArea";
 import { Link } from 'react-router-dom';
+import { useAxios } from "../../hooks/useAxios";
 
 
 const PieDataCategories = {
@@ -18,7 +18,7 @@ const PieDataCategories = {
   datasets: [
     {
       labels: '# of Votes',
-      data: [12, 19 ],
+      data: [12, 19],
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
         'rgba(54, 162, 235, 0.2)',
@@ -37,30 +37,7 @@ const PieDataCategories = {
     },
   ],
 };
-const PieData = {
-  labels: ['Crianças', 'Adolescentes', 'Jovens', 'Adultos', 'Idosos'],
-  datasets: [
-    {
-      labels: '# of Votes',
-      data: [12, 19, 3, 5, 2],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
+
 
 const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 const LineData = {
@@ -86,23 +63,103 @@ const LineData = {
 
 
 const OverviewPersons = () => {
+  const { data: dataPerson } = useAxios('person')
+
+  let homens = dataPerson?.person.map((person: any, index: any) => {
+    return person.sex === 'masculino' ? person.sex : 0
+  })
+  let mens = homens?.filter((i: any) => i === 'masculino').length
+
+  let mulheres = dataPerson?.person.map((person: any, index: any) => {
+    return person.sex === 'feminino' ? person.sex : 0
+  })
+  let mulh = mulheres?.filter((i: any) => i === 'feminino').length
+  var date = new Date()
+  var day = date.getDate()
+  var month = date.getMonth() + 1
+  var year = date.getFullYear()
+
+  let Age = dataPerson?.person.map((person: any,
+    index: any) => {
+    var BirthYear = new Date(person.birth)
+    var age = year - BirthYear.getFullYear()
+    var mont = month - BirthYear.getFullYear()
+    return mont < 0 || (mont === 0 && day < BirthYear.getDate()) ? age-- : age
+  })
+  let AgeGroup = Age.map((age: any) => {
+    let Group = ''
+    if (age >= 0 && age <= 12) {
+      Group = 'Criança'
+    }
+    if (age >= 12 && age <= 18) {
+      Group = "Adolescente"
+    }
+    if (age >= 18 && age <= 28) {
+      Group = "Jovem"
+    }
+    if (age >= 28 && age <= 55) {
+      Group = "Adulto"
+    }
+    if (age >= 55 && age <= 145) {
+      Group = "Idoso"
+    }
+
+    return Group
+  })
+
+  let child = AgeGroup?.map((agp: any, index: any) => { return agp === 'Criança' ? agp : 0 })
+  let adolescent = AgeGroup?.map((agp: any, index: any) => { return agp === 'Alolescente' ? agp : 0 })
+  let young = AgeGroup?.map((agp: any, index: any) => { return agp === 'Jovem' ? agp : 0 })
+  let adults = AgeGroup?.map((agp: any, index: any) => { return agp === 'Adulto' ? agp : 0 })
+  let seniors = AgeGroup?.map((agp: any, index: any) => { return agp === 'Idoso' ? agp : 0 })
+
+  let Crianca = child?.filter((i: any) => i === 'Criança').length
+  let Adolescente = adolescent?.filter((i: any) => i === 'Adolescente').length
+  let Jovem = young?.filter((i: any) => i === 'Jovem').length
+  let Adulto = adults?.filter((i: any) => i === 'Adulto').length
+  let Idoso = seniors?.filter((i: any) => i === 'Idoso').length
+  const PieData = {
+    labels: ['Crianças', 'Adolescentes', 'Jovens', 'Adultos', 'Idosos'],
+    datasets: [
+      {
+        labels: '# of Votes',
+        data: [Crianca, Adolescente, Jovem, Adulto, Idoso],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
   return (
     <Content>
       <Block>
         <Grid1>
           <Div>
             <FiUsers size="25" />
-            <h3>1</h3>
-            <p>Total de pessoas</p>
+            <p>Total de pessoas:</p>
+            <h3>{dataPerson?.person.length}</h3>
           </Div>
           <Div>
             <FiUser size="25" />
-            <h3>0.0% <span>(0)</span> </h3>
+
+            <h3>{mens / dataPerson?.person.length * 100}% <span>({mens})</span> </h3>
             <p>Total de Homens</p>
           </Div>
           <Div>
             <FiUser size="25" />
-            <h3>0.0% <span>(0)</span></h3>
+            <h3>{mulh / dataPerson?.person.length * 100}% <span>({mulh})</span></h3>
             <p>Total de Mulheres </p>
           </Div>
         </Grid1>
@@ -133,9 +190,9 @@ const OverviewPersons = () => {
               <Button><Link to='/categories' >Ver Todos</Link></Button>
             </BoxHeader>
             <BoxContent>
-              <Graph1>
+              <GraphContainer>
                 <GraphPieArea data={PieDataCategories} />
-              </Graph1>
+              </GraphContainer>
             </BoxContent>
           </Box>
         </Content>
