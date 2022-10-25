bffdf55365f2aee5ceeec192fa4f821fde0019e5
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-10-24 22:00:32 -0300

    Crud Financial

--- src/Components/MakeRevenues/index.tsx
+++ src/Components/MakeRevenues/index.tsx
@@ -1,87 +1,179 @@
 import React from 'react';
-import { AiOutlineExclamationCircle } from 'react-icons/ai';
+import { AiOutlineExclamationCircle, AiOutlineCloseCircle } from 'react-icons/ai';
 import { GiConfirmed } from 'react-icons/gi';
+import { FinancialContext } from '../../contexts/financialContext';
+import { useAxios } from '../../hooks/useAxios';
+import Block from '../Block';
 import BoxContent from '../BoxContent';
 import BoxHeader from '../BoxHeader';
+import Button from '../Button';
+import Flex from '../Flex';
 import FlexBetween from '../FlexBetween';
 import Input from '../Input';
 import InputBlock from '../InputBlock';
 
-import { Container, Content, Close } from './styles';
-interface Props {
-  children?: React.ReactNode;
-  onclick?: () => void;
-}
-const MakeRevenues = ({children,onclick}:Props) => {
+import { Container, Form, Close } from './styles';
+const MakeRevenues = () => {
+  const [paid, setPaid] = React.useState(false)
+
+  function paidToggle() {
+    setPaid(paid === true ? false : true)
+  }
+  React.useEffect(() => {
+    if (paid === false) {
+      setPaidOut('Pago')
+    } else {
+      setPaidOut('Pendente')
+    }
+  })
+  const { data: dataPerson } = useAxios('person')
+  const { data: dataCategory } = useAxios('category')
+
+  const {
+    date,
+    description,
+    value,
+    setPaidOut,
+    receivedFrom,
+    category,
+    account,
+    costCenter,
+    typeOfPayment,
+    documentNumber,
+    competence,
+    notes,
+    file,
+    dateHandler,
+    descriptionHandler,
+    valueHandler,
+    receivedFromHandler,
+    categoryHandler,
+    accountHandler,
+    costCenterHandler,
+    typeOfPaymentHandler,
+    documentNumberHandler,
+    competenceHandler,
+    notesHandler,
+    fileHandler,
+    handleSubmit,
+    setModal,
+    revenuesExpenses,
+    handleDelete,
+    id
+  } = React.useContext(FinancialContext)
+  console.log(revenuesExpenses)
   return (
     <Container>
-      <Content>
-        <BoxHeader title='Criar Receita'>{children}</BoxHeader>
+      <Form onSubmit={handleSubmit}>
+        <BoxHeader title={revenuesExpenses ? 'Criar Receita' : 'Criar Despesa'}>
+          <Button type='button' onClick={() => setModal(false)}><AiOutlineCloseCircle size={30} /></Button>
+          {id ? <Button onClick={() => handleDelete(id)}>Apagar</Button> : null}
+        </BoxHeader>
         <BoxContent>
-          <FlexBetween>
+          <Flex>
             <InputBlock>
               <label htmlFor="data">Data</label>
-              <Input type='date' />
+              <Input type='date' value={date} onChange={dateHandler} />
             </InputBlock>
             <InputBlock>
               <label htmlFor="description">Descrição</label>
-              <Input type='text' />
+              <Input type='text' value={description} onChange={descriptionHandler} />
             </InputBlock>
             <InputBlock>
               <label htmlFor="value">Valor</label>
-              <Input type='text' placeholder='0.00'/>
+              <Input type='number' placeholder='0.00' value={value} onChange={valueHandler} />
             </InputBlock>
             <InputBlock>
               <label htmlFor="pay">Pago?</label>
-              <GiConfirmed />
-              <AiOutlineExclamationCircle />
+              {paid ?
+                <Button type='button' onClick={() => paidToggle()}>
+                  <AiOutlineExclamationCircle size={30} color='#f00' />
+                </Button>
+
+                :
+                <Button type='button' onClick={() => paidToggle()}>
+                  <GiConfirmed size={30} color='#0f0' />
+                </Button>
+              }
             </InputBlock>
-          </FlexBetween>
-          <FlexBetween>
+          </Flex>
+          <Flex>
             <InputBlock>
               <label htmlFor="recibo">Recebido de</label>
-              <select title='recibo'>
+              <select title='recibo' value={receivedFrom} onChange={receivedFromHandler}>
                 <option value='' ></option>
+                {dataPerson?.person.map((person: any, index: any) => {
+                  return (
+
+                    <option value={person.name} >{person.name}</option>
+                  )
+                })}
               </select>
             </InputBlock>
             <InputBlock>
               <label htmlFor="description">Categoria</label>
-              <select title='recibo'>
+              <select title='recibo' value={category} onChange={categoryHandler}>
                 <option value='' ></option>
+                {dataCategory?.category.map((category: any,
+                  index: number) => {
+                  return (
+
+                    <option key={index} value={category.categoryName} >{category.categoryName}</option>
+                  )
+                })}
               </select>
             </InputBlock>
             <InputBlock>
               <label htmlFor="value">Conta</label>
-              <select title='recibo'>
+              <select title='recibo' value={account} onChange={accountHandler}>
                 <option value='' ></option>
               </select>
             </InputBlock>
             <InputBlock>
               <label htmlFor="pay">Centro de custo</label>
-              <select title='recibo'>
+              <select title='recibo' value={costCenter} onChange={costCenterHandler}>
                 <option value='' ></option>
               </select>
             </InputBlock>
-          </FlexBetween>
-          <FlexBetween>
+          </Flex>
+          <Flex>
+            <Block>
+              <Flex>
+                <InputBlock>
+                  <label htmlFor="recibo">Tipo de pagamento</label>
+                  <select title='recibo' value={typeOfPayment} onChange={typeOfPaymentHandler}>
+                    <option value='Unico' >Único</option>
+                    <option value='Parcela' >Parcela</option>
+                  </select>
+                </InputBlock>
+                <InputBlock>
+                  <label htmlFor="description">Doc nº</label>
+                  <Input type='text' value={documentNumber} onChange={documentNumberHandler} />
+                </InputBlock>
+              </Flex>
+              <Flex>
+                <InputBlock>
+                  <label htmlFor="competence">Competencia</label>
+                  <Input type='date' value={competence} onChange={competenceHandler} />
+                </InputBlock>
+                <InputBlock>
+                  <label htmlFor="file">Anexar Arquivo</label>
+                  <Input type='file' value={file} onChange={fileHandler} />
+                </InputBlock>
+              </Flex>
+            </Block>
             <InputBlock>
-              <label htmlFor="recibo">Tipo de pagamento</label>
-              <select title='recibo'>
-                <option value='' ></option>
-              </select>
+              <label htmlFor="value">Anotações</label>
+              <textarea title="text" id="" value={notes} onChange={notesHandler}></textarea>
             </InputBlock>
-            <InputBlock>
-              <label htmlFor="description">Doc nº</label>
-              <Input type='text' />
-            </InputBlock>
-            <InputBlock>
-              <label htmlFor="value">Conta</label>
-              <textarea title="text" id="" cols={30} rows={10}></textarea>
-            </InputBlock>
-          </FlexBetween>
+          </Flex>
+          <Flex>
+            <Button type='submit' >Salvar e novo</Button>
+            <Button type='submit' >Salvar e fechar</Button>
+          </Flex>
         </BoxContent>
-      </Content>
-      <Close onClick={onclick}></Close>
+      </Form>
+      <Close onClick={() => setModal(false)}></Close>
     </Container>
   )
 }
