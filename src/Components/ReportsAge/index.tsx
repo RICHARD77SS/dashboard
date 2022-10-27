7d427bee029aa95af7303c3d8a7c02961c3984d6
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-10-26 21:08:18 -0300

    Inserção dos dados no grafico e tabela

--- src/Components/ReportsAge/index.tsx
+++ src/Components/ReportsAge/index.tsx
@@ -1,3 +1,4 @@
+import { useAxios } from '../../hooks/useAxios';
 import Button from '../Button';
 import GraphPieArea from '../GraphPieArea';
 import ReportsHeader from '../ReportsHeader';
@@ -10,38 +11,82 @@ import Tr from '../Tr';
 import { Container, GraphContainer } from './styles';
 
 
-const children = 2
-const teenagers = 10
-const youngPeople = 8
-const adults = 5
-const seniors = 4
 
 
-const PieData = {
-  labels: ['Crianças', 'Adolescentes', 'Jovens', 'Adultos', 'Idosos'],
-  datasets: [
-    {
-      labels: '# of Votes',
-      data: [children, teenagers, youngPeople, adults, seniors],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
+
 const ReportsAge = () => {
+  const { data: dataPerson } = useAxios('person')
+  var date = new Date()
+  var day = date.getDate()
+  var month = date.getMonth() + 1
+  var year = date.getFullYear()
+
+  let Age = dataPerson?.person.map((person: any,
+    index: number) => {
+    var BirthYear = new Date(person.birth)
+    var age = year - BirthYear.getFullYear()
+    var mont = month - BirthYear.getFullYear()
+    return mont < 0 || (mont === 0 && day < BirthYear.getDate()) ? age-- : age
+  })
+
+  let AgeGroup = Age?.map((age: any) => {
+    let Group = ''
+    if (age >= 0 && age <= 12) {
+      Group = 'Criança'
+    }
+    if (age >= 12 && age <= 18) {
+      Group = "Adolescente"
+    }
+    if (age >= 18 && age <= 28) {
+      Group = "Jovem"
+    }
+    if (age >= 28 && age <= 55) {
+      Group = "Adulto"
+    }
+    if (age >= 55 && age <= 145) {
+      Group = "Idoso"
+    }
+
+    return Group
+  })
+
+  let child = AgeGroup?.map((agp: string, index: number) => { return agp === 'Criança' ? agp : 0 })
+  let adolescent = AgeGroup?.map((agp: string, index: number) => { return agp === 'Alolescente' ? agp : 0 })
+  let young = AgeGroup?.map((agp: string, index: number) => { return agp === 'Jovem' ? agp : 0 })
+  let adults = AgeGroup?.map((agp: string, index: number) => { return agp === 'Adulto' ? agp : 0 })
+  let seniors = AgeGroup?.map((agp: string, index: number) => { return agp === 'Idoso' ? agp : 0 })
+
+  let Crianca = child?.filter((i: string) => i === 'Criança').length
+  let Adolescente = adolescent?.filter((i: string) => i === 'Adolescente').length
+  let Jovem = young?.filter((i: string) => i === 'Jovem').length
+  let Adulto = adults?.filter((i: string) => i === 'Adulto').length
+  let Idoso = seniors?.filter((i: string) => i === 'Idoso').length
+
+  const PieData = {
+    labels: ['Crianças', 'Adolescentes', 'Jovens', 'Adultos', 'Idosos'],
+    datasets: [
+      {
+        labels: '# of Votes',
+        data: [Crianca, Adolescente, Jovem, Adulto, Idoso],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return (
     <Container>
 
@@ -60,27 +105,27 @@ const ReportsAge = () => {
         <Tbody>
           <Tr>
             <Td>Crianças</Td>
-            <Td>{children}</Td>
+            <Td>{Crianca}</Td>
             <Td><Button>Ver pessoas</Button></Td>
           </Tr>
           <Tr>
             <Td>Adolescentes</Td>
-            <Td>{teenagers}</Td>
+            <Td>{Adolescente}</Td>
             <Td><Button>Ver pessoas</Button></Td>
           </Tr>
           <Tr>
             <Td>Jovens</Td>
-            <Td>{youngPeople}</Td>
+            <Td>{Jovem}</Td>
             <Td><Button>Ver pessoas</Button></Td>
           </Tr>
           <Tr>
             <Td>Adultos</Td>
-            <Td>{adults}</Td>
+            <Td>{Adulto}</Td>
             <Td><Button>Ver pessoas</Button></Td>
           </Tr>
           <Tr>
             <Td>Idosos</Td>
-            <Td>{seniors}</Td>
+            <Td>{Idoso}</Td>
             <Td><Button>Ver pessoas</Button></Td>
           </Tr>
         </Tbody>
