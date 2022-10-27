b8bd89f9c7959b0d206ca8b2d7f2f80074ba1da0
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-10-27 20:50:56 -0300

    Adição dos dados de cargos na tabela e no grafico

--- src/Components/ReportOffice/index.tsx
+++ src/Components/ReportOffice/index.tsx
@@ -1,9 +1,20 @@
 import Button from '../Button';
-import {  Container,Content,Table,Thead,Tr,Th,Tbody,Td } from './styles';
+import { Container, Content } from './styles';
 import { BsArrowUp, BsArrowDown } from 'react-icons/bs';
 import ReportsHeader from '../ReportsHeader';
+import { useAxios } from '../../hooks/useAxios';
+import Table from '../Table';
+import Thead from '../Thead';
+import Tr from '../Tr';
+import Th from '../Th';
+import Tbody from '../Tbody';
+import Td from '../Td';
 
 const ReportsOffice = () => {
+  const { data } = useAxios('positions')
+  const { data: dataPerson } = useAxios('person')
+  let offices = dataPerson?.person.map((person: any) => person.office)
+  console.log(dataPerson?.person.map((person: any) => person.office))
   return (
     <Container>
       <ReportsHeader logo='' corporation='Inc name' reportsName='Cargos' />
@@ -16,10 +27,15 @@ const ReportsOffice = () => {
             </Tr>
           </Thead>
           <Tbody>
-            <Tr>
-              <Td>Name here</Td>
-              <Td>Email here</Td>
-            </Tr>
+            {data?.positions.map((positions: any, index: number) => {
+              return (
+                <Tr>
+                  <Td>{positions.name}</Td>
+                  <Td>{offices?.filter((i: string) => i === positions.name).length}</Td>
+                </Tr>
+
+              )
+            })}
           </Tbody>
         </Table>
       </Content>
