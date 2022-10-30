162a41a5ccd47ece8eb770ba713d82401071a38f
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-10-30 17:30:39 -0300

    Inserção dos dados na tabela

--- src/Components/GroupReportsNewGroups/index.tsx
+++ src/Components/GroupReportsNewGroups/index.tsx
@@ -13,9 +13,12 @@ import { Container, Content, Top, Block } from './styles';
 import { BsArrowDown, BsArrowUp } from 'react-icons/bs';
 import Table from '../Table';
 import PageSelector from '../PageSelector';
+import { useAxios } from '../../hooks/useAxios';
 
 
 const GroupReportsNewGroups = () => {
+  const { data: dataPerson } = useAxios('person')
+  const { data } = useAxios('groups')
   return (
     <Container>
       <ReportsHeader logo='' corporation='Inc name' reportsName='Novos Grupos' />
@@ -24,7 +27,7 @@ const GroupReportsNewGroups = () => {
           <Block>
             <h3>Resultados: 1 Grupo</h3>
             <p>Grupos iniciados nos últimos 60 dias</p>
-          </Block>          
+          </Block>
         </Top>
         <TopTableOptions />
         <Input placeholder=' Buscar' type='search' />
@@ -38,12 +41,18 @@ const GroupReportsNewGroups = () => {
             </Tr>
           </Thead>
           <Tbody>
-            <Tr>
-              <Td><img src="" alt="" /></Td>
-              <Td>Data here</Td>
-              <Td>Grupo here</Td>
-              <Td>Valor total here</Td>
-            </Tr>
+            {data?.groups.map((group: any, index: number) => {
+              let personsInGroup = dataPerson?.person.map((person: any) => person.group)?.filter((groupName: any) => groupName === group.name).length
+              return (
+                <Tr key={index}>
+                  <Td><img src={group.image} alt="" /></Td>
+                  <Td>{group.creationDate.split('T')[0]}</Td>
+                  <Td>{group.name}</Td>
+                  <Td>{personsInGroup}</Td>
+                </Tr>
+
+              )
+            })}
           </Tbody>
         </Table>
         <PageSelector />
