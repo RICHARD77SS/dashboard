05ba22bf4c635fe9f0f463e83079c7377603ba2c
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-11-07 22:04:25 -0300

    Estilos para lista e formulario

--- src/Components/AddMeetings/styles.ts
+++ src/Components/AddMeetings/styles.ts
@@ -19,11 +19,32 @@ export const ModalContent = styled.div`
   flex-direction: column;
   border-radius: 10px;
   padding: 1rem;
+  gap: 1rem;
   z-index: 30;
   background: ${Props => Props.theme.colors.bac};
+  
+  button {
+    background: ${Props => Props.theme.colors.pri};
+    padding: .5rem 1rem;
+    border-radius: 10px;
+    color: #fff;
+  }
 `
 export const Form = styled.form`
-
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`
+export const List = styled.form`
+  width: auto;
+  min-height: 200px;
+  padding: .5rem;
+  border: 1px solid ${Props => Props.theme.colors.sec};
+  border-radius: 20px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 `
 export const CloseModal = styled.button`
   position: fixed;
