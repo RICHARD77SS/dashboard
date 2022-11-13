56d436cb1f5cd2a05a6d976c4414b6d38fe6a63a
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-11-12 22:54:27 -0300

    Dados de conversão de usuarios

--- src/Components/ReportsConversion/index.tsx
+++ src/Components/ReportsConversion/index.tsx
@@ -1,18 +1,27 @@
-
+import React from 'react'
 import { useAxios } from '../../hooks/useAxios';
+import { NowDate } from '../../utils/getDate';
 import GraphLineArea from '../GraphLineArea';
 import Input from '../Input';
 import ReportsHeader from '../ReportsHeader';
-import {  Container,Content,Graph,  Table,Thead,Tr,Th,Tbody,Td } from './styles';
+import Table from '../Table';
+import Tbody from '../Tbody';
+import Td from '../Td';
+import Th from '../Th';
+import Thead from '../Thead';
+import Tr from '../Tr';
+import { Container, Content, Graph } from './styles';
 
 
 
 const ReportsConversion = () => {
-  const {data}=useAxios('person')
-  let conversionDate = data?.person.map((person: any) => person.conversion)
-  console.log(conversionDate)
+  const { data } = useAxios('person')
+  let conversionDate = data?.person.map((person: any) => person.conversion.split('T')[0])
 
-  
+  const [now, setNow] = React.useState(NowDate)
+  function nowHandler(event: any) {
+    setNow(event.target.value)
+  }
   const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   const LineData = {
     labels,
@@ -20,7 +29,7 @@ const ReportsConversion = () => {
       {
         fill: true,
         label: 'conversão',
-        data: [1222, 212, 313, 414, 166, 778, 919],
+        data: [0,0,0,0,0,0,0],
         borderColor: 'rgb(53, 162, 235)',
         backgroundColor: 'rgba(53, 162, 235, 0.5)',
       },
@@ -30,9 +39,9 @@ const ReportsConversion = () => {
     <Container>
       <ReportsHeader logo='' corporation='Inc name' reportsName='Data de conversão' />
       <Content>
-        <Input type='data' />
+        <Input type='date' value={now} onChange={nowHandler} />
         <Graph>
-          <GraphLineArea data={LineData}/>
+          <GraphLineArea data={LineData} />
         </Graph>
         <h3>Resultados: 0</h3>
         <Table>
@@ -44,11 +53,15 @@ const ReportsConversion = () => {
             </Tr>
           </Thead>
           <Tbody>
-            <Tr>
-              <Td><img src="" alt="" /></Td>
-              <Td>User name</Td>
-              <Td>08/09/2022</Td>
-            </Tr>
+            {data?.person.map((person: any) => {
+              return (
+                <Tr>
+                  <Td><img src={person.image} alt="" /></Td>
+                  <Td>{person.name}</Td>
+                  <Td>{person.conversion.split('T')[0]}</Td>
+                </Tr>
+              )
+            })}
 
           </Tbody>
         </Table>
