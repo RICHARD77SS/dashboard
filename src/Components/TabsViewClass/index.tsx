7d06b0001e905419c6bcb4a38ac68b4303a50a02
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-11-18 22:08:23 -0300

    Tabs com infos das salas

---
+++ src/Components/TabsViewClass/index.tsx
@@ -0,0 +1,58 @@
+import TabTitle from '../TabTitle';
+import { useState, ReactElement } from 'react';
+import { TabsButtons, TabContainer } from './styles'
+interface Props {
+  children: ReactElement[]
+}
+
+const TabsViewClass = (Props: Props) => {
+  const [selectedTab, setSelectedTab] = useState(0)
+
+  if (selectedTab === 0) {
+    document.querySelector('.Informações')?.classList.add('active')
+  } else {
+    document.querySelector('.Informações')?.classList.remove('active')
+  }
+  if (selectedTab === 1) {
+    document.querySelector('.Campos')?.classList.add('active')
+  } else {
+    document.querySelector('.Campos')?.classList.remove('active')
+  }
+  if (selectedTab === 2) {
+    document.querySelector('.Financeiro')?.classList.add('active')
+  } else {
+    document.querySelector('.Financeiro')?.classList.remove('active')
+  }
+  if (selectedTab === 3) {
+    document.querySelector('.Editar')?.classList.add('active')
+  } else {
+    document.querySelector('.Editar')?.classList.remove('active')
+  }
+  if (selectedTab === 4) {
+    document.querySelector('.Permissões')?.classList.add('active')
+  } else {
+    document.querySelector('.Permissões')?.classList.remove('active')
+  }
+  if (selectedTab === 5) {
+    document.querySelector('.Remover')?.classList.add('active')
+  } else {
+    document.querySelector('.Remover')?.classList.remove('active')
+  }
+  return (
+    <TabContainer>
+      <TabsButtons>
+        {Props.children.map((item, index) => (
+          <TabTitle
+            key={index}
+            title={item.props.title}
+            index={index}
+            setSelectedTab={setSelectedTab}
+          />
+        ))}
+      </TabsButtons>
+      {Props.children[selectedTab]}
+    </TabContainer>
+  )
+}
+
+export default TabsViewClass;
---
+++ src/Components/TabsViewClass/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const TabContainer = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  align-items:Center;
+  flex-direction: column;
+  background: ${Props => Props.theme.colors.bac};
+  padding: 1rem;
+  gap: 1rem;
+  border-radius: 10px;
+
+`
+export const TabsButtons = styled.div`
+  width: 100%;
+  display: flex;
+  align-items:Center;
+  padding: 1rem 2rem;
+  background: ${Props => Props.theme.colors.pri};
+  gap: 1rem;
+  border-radius: 10px;
+  flex-wrap: wrap; 
+  .active {
+    background: ${Props => Props.theme.colors.sec};
+    color: ${Props => Props.theme.colors.bac};
+  }
+`
