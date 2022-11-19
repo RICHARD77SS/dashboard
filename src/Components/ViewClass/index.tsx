6df150a58a02cda0641e41600bea495d79030983
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-11-18 22:07:40 -0300

    Ver classes e editar infos

---
+++ src/Components/ViewClass/index.tsx
@@ -0,0 +1,169 @@
+import React from 'react'
+import { ClaassContext } from '../../contexts/claassContext'
+import { useAxios } from '../../hooks/useAxios'
+import Button from '../Button'
+import Container from '../Container'
+import Content from '../Content'
+import Flex from '../Flex'
+import Input from '../Input'
+import InputBlock from '../InputBlock'
+import Tab from '../Tab'
+import TabsViewClass from '../TabsViewClass'
+
+import { Form, Box, Modal, ModalContent, CloseModal, User } from './styles'
+import { Link, useParams } from 'react-router-dom';
+
+const ViewClass = () => {
+  const { data: dataSchools } = useAxios('schools')
+  const { data: dataPerson } = useAxios('person')
+  const { data: dataClass } = useAxios('claass')
+  const {
+    name,
+    school,
+    time,
+    status,
+    description,
+    nameHandler,
+    schoolHandler,
+    timeHandler,
+    statusHandler,
+    descriptionHandler,
+    handleSubmit,
+    openModal,
+    setOpenModal,
+    participants,
+    participantsHandler,
+    setName,
+    setDescription,
+    setSchool,
+    setTime,
+    setStatus,
+    setParticipants,
+    setId
+  } = React.useContext(ClaassContext)
+  let { id } = useParams()
+
+
+  React.useEffect(() => {
+    setName(dataClass?.claass[`${id}`].name)
+    setDescription(dataClass?.claass[`${id}`].description)
+    setSchool(dataClass?.claass[`${id}`].school)
+    setTime(dataClass?.claass[`${id}`].time)
+    setStatus(dataClass?.claass[`${id}`].status)
+    setParticipants(dataClass?.claass[`${id}`].participants)
+    setId(dataClass?.claass[`${id}`]._id)
+
+  }, [dataClass?.claass, id, setDescription, setName, setParticipants, setSchool, setStatus, setTime, setId])
+  return (
+    <Container>
+      <Content>
+        <br />
+        <Form onSubmit={handleSubmit}>
+          <InputBlock>
+            <label htmlFor='scoolName'>Nome da turma</label>
+            <Input type='text' id='scoolName' value={name} onChange={nameHandler} />
+          </InputBlock>
+          <InputBlock>
+            <label>Escola</label>
+            <select title='schools' value={school} onChange={schoolHandler}>
+              <option value=""></option>
+              {dataSchools?.schools.map((schools: any, index: number) => {
+                return (
+                  <option key={index} value={schools.name} >{schools.name}</option>
+                )
+              })}
+            </select>
+          </InputBlock>
+          <InputBlock>
+            <label>Horário</label>
+            <select title='hours' value={time} onChange={timeHandler}>
+              <option value="" ></option>
+              <option value="Não definido" >Não definido</option>
+              <option value="Manhã" >Manhã</option>
+              <option value="Tarde" >Tarde</option>
+              <option value="Noite">Noite</option>
+            </select>
+          </InputBlock>
+          <InputBlock>
+            <label>Status</label>
+            <select title='status' value={status} onChange={statusHandler}>
+              <option value=""></option>
+              <option value="Preparando turma">Preparando turma</option>
+              <option value="Em andamento" >Em andamento</option>
+              <option value="Pausada" >Pausada</option>
+              <option value="Finalizada">Finalizada</option>
+            </select>
+          </InputBlock>
+          <InputBlock>
+            <label htmlFor="description">Descrição</label>
+            <textarea title='textarea' name="" id="description" cols={30} rows={10} value={description} onChange={descriptionHandler}></textarea>
+          </InputBlock>
+          <Button type='submit'>Salvar</Button>
+        </Form>
+        {openModal ?
+          <>
+            <Modal onSubmit={handleSubmit}>
+              <ModalContent>
+
+                <Button type='button' onClick={() => setOpenModal(false)}>Close</Button>
+                <br />
+                <br />
+                {dataPerson?.person.map((person: any, index: number) => {
+                  return (
+                    <Flex key={index}>
+                      <Input type='checkbox' id={person.name} name={person.name} value={index} checked={participants.includes(`${index}`) ? true : false} onChange={participantsHandler} />
+                      <label htmlFor={person.name}>{person.name}</label>
+                    </Flex>
+                  )
+                })}
+                <Button type='submit'>Adicionar</Button>
+              </ModalContent>
+            </Modal>
+            <CloseModal type='button' onClick={() => setOpenModal(false)}></CloseModal>
+          </>
+          : null}
+        <Box>
+          <TabsViewClass>
+            <Tab title='Alunos'>
+              <Box>
+                <Button className='buttonadd' type='button' onClick={() => setOpenModal(true)}>+ Adicionar/Remover Aluno</Button>
+                <br />
+                <InputBlock>
+                  {dataClass?.claass[`${id}`].participants.map((participant: any, index: number) => {
+
+                    return (
+                      <User key={index}>
+                        <img src={dataPerson?.person[participant].image ? dataPerson?.person[participant].image : 'https://img.freepik.com/psd-gratuitas/ilustracao-3d-de-uma-pessoa-com-oculos-de-sol_23-2149436188.jpg?w=740&t=st=1668802478~exp=1668803078~hmac=58cc3851b15b54d4cf8ceda77ebf6bb89cde07fe2b3e6906b60b673a20018edf'} alt="" />
+                        <Link to={`/persons/details/${participant}`}><h3>{dataPerson?.person[participant].name}</h3></Link>
+                      </User>
+                    )
+                  })}
+                </InputBlock>
+              </Box>
+            </Tab>
+            <Tab title='Disciplinas'>
+              <Box>
+                <Button className='buttonadd' type='button'>+ Adicionar Disciplina</Button>
+              </Box>
+            </Tab>
+            <Tab title='Aulas'>
+              <Box>
+                <Button className='buttonadd' type='button'>+ Adicionar Aula</Button>
+              </Box>
+            </Tab>
+            <Tab title='Arquivos'>
+              <Box>
+                <Button className='buttonadd' type='button'>+ Adicionar</Button>
+              </Box>
+            </Tab>
+            <Tab title='Relatórios'>
+
+            </Tab>
+          </TabsViewClass>
+        </Box>
+      </Content>
+    </Container>
+  )
+}
+
+export default ViewClass;
---
+++ src/Components/ViewClass/styles.ts
@@ -0,0 +1,119 @@
+import styled from 'styled-components'
+
+export const Form = styled.form`
+  width: 300px;
+  height: auto;
+  display: flex;
+  align-items:Center;
+  justify-content: center;
+  flex-direction: column;
+  background: #fff;
+  gap: 1rem;
+  padding: 1rem;
+  select,input {
+    width: 260px;
+    height: 30px;
+    border-radius: 5px;
+    border-color: #eee;
+  }
+  textarea{
+    max-width: 290px;
+    max-height: 300px;
+  }
+`
+export const Box = styled.div`
+  width: 100%;
+  height: auto;
+  background: #fff;
+  display: flex;
+  align-items:center;
+  justify-content:center;
+  flex-direction: column;
+  .navlink {
+    flex:1;
+    padding: 0 .5rem ;
+    height: 60px;
+    display: flex;
+    align-items:center;
+    justify-content:center;
+    border-radius: 5px;
+    background: #E0FFFF;
+    color: #000;
+    text-decoration: none;
+    font-size: 1rem;
+  }
+  .navlink.active{
+    background: ${Props => Props.theme.colors.pri};
+    color: #fff;
+  }
+  .buttonadd{
+    padding: .5rem 1rem;
+    background: ${Props => Props.theme.colors.sec};
+    border-radius: 20px;
+    color:${Props => Props.theme.colors.bac};
+    display: flex;
+    align-items:center;
+    justify-content:center;
+  }
+`
+export const Modal = styled.form`
+  width: 400px;
+  height: 300px;
+  display: flex;
+  align-items:center;
+  flex-direction: column;
+  gap: .5rem;
+  z-index: 20;
+  position: absolute;
+`
+export const ModalContent = styled.div`
+  width: 100%;
+  height: 100%;
+  z-index: 20;
+  background:${Props => Props.theme.colors.bac};
+  border-radius: 10px;
+  padding: 1rem;
+  gap: 1rem;
+  button{
+    padding: .5rem 1rem;
+    background: #900;
+    border-radius: 20px;
+    color:${Props => Props.theme.colors.bac};
+    display: flex;
+    align-items:center;
+    justify-content:center;
+  }
+
+`
+export const CloseModal = styled.button`
+  position: fixed;
+  display: flex;
+  align-items:center;
+  justify-content:center;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: rgba(0,0,0,0.4);
+  z-index: 10;
+`
+export const User = styled.button`
+  width: 100%;
+  height: 60px;
+  display: flex;
+  align-items:center;
+  justify-content:start;
+  gap: .5rem;
+  background: #eee;
+  border-radius: 10px;
+  padding: .5rem;
+  border: none;
+  img{
+    width: 60px;
+    height: 50px;
+    border-radius: 5px;
+  }
+  a{
+    color:#000;
+  }
+`
