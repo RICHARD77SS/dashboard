1ec8092126566c83b6d59400d72925532e9dfa60
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-11-24 19:38:54 -0300

    Mostra os dados das escolas na tabela e as pessoas para adicionar como gestores

--- src/Components/Schools/index.tsx
+++ src/Components/Schools/index.tsx
@@ -1,10 +1,13 @@
-import Aside from '../Aside';
+import React from 'react';
+import { SchoolsContext } from '../../contexts/schoolsContext';
+import { useAxios } from '../../hooks/useAxios';
 import Box from '../Box';
 import BoxContent from '../BoxContent';
 import BoxHeader from '../BoxHeader';
 import Button from '../Button';
 import Container from '../Container';
 import EditRemove from '../EditRemove';
+import Flex from '../Flex';
 import Input from '../Input';
 import Table from '../Table';
 import Tbody from '../Tbody';
@@ -12,9 +15,20 @@ import Td from '../Td';
 import Th from '../Th';
 import Thead from '../Thead';
 import Tr from '../Tr';
-import { Content, InputBox } from './styles';
+import { Content, InputBox, Aside, List } from './styles';
 
 const Schools = () => {
+  const { data: dataSchools } = useAxios('schools')
+  const { data: dataPerson } = useAxios('person')
+  const { name,
+    description,
+    nameHandler,
+    descriptionHandler,
+    managersHandler,
+    handleSubmit,
+    handleDelete,
+    handleEdit,
+  } = React.useContext(SchoolsContext)
   return (
     <Container>
       <h3>Escolas</h3>
@@ -23,7 +37,7 @@ const Schools = () => {
           <BoxHeader title={`Resultados: 0`}>
 
           </BoxHeader>
-          <BoxContent>
+          <BoxContent width='100%'>
             <Table>
               <Thead>
                 <Tr>
@@ -32,31 +46,54 @@ const Schools = () => {
                 </Tr>
               </Thead>
               <Tbody>
-                <Tr>
-                  <Td>NameHere</Td>
-                  <Td><EditRemove /></Td>
-                </Tr>
+                {dataSchools?.schools.map((schools: any, index: number) => {
+                  return (
+                    <Tr key={index}>
+                      <Td>{schools.name}</Td>
+                      <Td>
+                        <Flex>
+                          <Button onClick={() => handleEdit(schools._id, schools.name, schools.description, schools.managers)}>Editar</Button>
+                          <Button onClick={() => handleDelete(schools._id)}>Apagar</Button>
+                        </Flex>
+                      </Td>
+                    </Tr>
+
+                  )
+                })}
               </Tbody>
             </Table>
           </BoxContent>
         </Box>
-        <Aside>
+        <Aside onSubmit={handleSubmit}>
           <BoxHeader title='+ Criar escola' />
-          <BoxContent>
+          <BoxContent width='100%'>
             <InputBox>
               <label htmlFor='scoolName'>Nome da escola</label>
-              <Input type='text' id='scoolName' />
+              <Input type='text' id='scoolName' value={name} onChange={nameHandler} />
             </InputBox>
-            <InputBox>
-              <label>Gestores</label>
-              <Input placeholder='  Selecione' id='mananger' type='text' list='manangers' />
-              <datalist id='manangers'>
-                <option value="Lider name here"></option>
-              </datalist>
-            </InputBox>
+            <List>
+              <h3>Gestores</h3>
+              <br />
+              {dataPerson?.person.map((person: any, index: number) => {
+                return (
+                  <Flex key={index}>
+                    <label htmlFor={person.name}>{person.name}</label>
+                    <Input id={person.name} type='checkbox' value={person.name} onChange={managersHandler} />
+                  </Flex>
+                )
+              })}
+            </List>
             <InputBox>
               <label htmlFor="description">Descrição</label>
-              <textarea title='textarea' name="" id="description" cols={30} rows={10}></textarea>
+              <textarea
+                title='textarea'
+                name=""
+                id="description"
+                cols={30}
+                rows={10}
+                value={description}
+                onChange={descriptionHandler}
+              ></textarea>
             </InputBox>
             <Button>Criar</Button>
           </BoxContent>
