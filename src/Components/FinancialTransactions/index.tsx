a4c02b1c28c05ed00c70df957aadf81d55d3e12a
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-12-01 20:58:04 -0300

    Filtro dia e ano

--- src/Components/FinancialTransactions/index.tsx
+++ src/Components/FinancialTransactions/index.tsx
@@ -26,80 +26,131 @@ import React from 'react';
 import { FinancialContext } from '../../contexts/financialContext';
 
 
-const LineData = {
-  labels: [
-    '01/01/2022',
-    '02/01/2022',
-    '03/01/2022',
-    '04/01/2022',
-    '05/01/2022',
-    '06/01/2022',
-    '07/01/2022',
-    '08/01/2022',
-    '09/01/2022',
-    '10/01/2022',
-    '11/01/2022',
-    '12/01/2022',
-    '13/01/2022',
-    '14/01/2022',
-    '15/01/2022',
-    '16/01/2022',
-    '17/01/2022',
-    '18/01/2022',
-    '19/01/2022',
-    '20/01/2022',
-    '21/01/2022',
-    '22/01/2022',
-    '23/01/2022',
-    '24/01/2022',
-    '25/01/2022',
-    '26/01/2022',
-    '27/01/2022',
-    '28/01/2022',
-    '29/01/2022',
-    '30/01/2022'
-  ],
-  datasets: [
-    {
-      fill: true,
-      label: 'Receitas',
-      data: [222, 212, 313, 414, 166, 778, 119, 0, 100, 200, 400, 500, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100],
-      borderColor: 'rgb(65, 176, 37)',
-      backgroundColor: 'rgba(32, 248, 3, 0.5)',
-    },
-    {
-      fill: true,
-      label: 'Despesas',
-      data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      borderColor: 'rgb(255, 15, 15)',
-      backgroundColor: 'rgba(255, 0, 0, 0.5)',
-    },
-    {
-      fill: true,
-      label: 'A receber',
-      data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      borderColor: 'rgb(53, 174, 235)',
-      backgroundColor: 'rgba(53, 181, 235, 0.5)',
-    },
-    {
-      fill: true,
-      label: 'A pagar',
-      data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      borderColor: 'rgb(235, 93, 53)',
-      backgroundColor: 'rgba(235, 99, 53, 0.5)',
-    },
-  ],
-};
+
 const FinancialTransactions = () => {
   const { handleEdit, handleDelete, OpenRevenues, OpenExpenses } = React.useContext(FinancialContext)
   const { data: dataFinancial } = useAxios('financial')
+
+  const dateNow = new Date()
+  var day = dateNow.getDate()
+  var month = dateNow.getMonth() + 1
+  var year = dateNow.getFullYear()
+
+  const [financialMonth, setFinancialMonth] = React.useState(month.toString())
+  const [financialYear, setFinancialYear] = React.useState(year.toString())
+
+  let date = new Date(`${financialYear}-${financialMonth}-01`)
+
+
+  function getAllDays(years: any, months: any) {
+    let date = new Date(years, months + 1, 1);
+    let dates = []
+
+    while (date.getMonth() === months + 1) {
+      dates.push(new Date(date));
+      date.setDate(date.getDate() + 1)
+    }
+    return dates
+  }
+
+  let daysInMonth = getAllDays(date.getFullYear(), date.getMonth())
+
+  console.log(daysInMonth)
+  function padTo2Digits(nume: any) {
+    return nume.toString().padStart(2, '0')
+  }
+  function formatDate(date: any) {
+    return [
+      padTo2Digits(date.getDate()),
+      padTo2Digits(date.getMonth() + 1),
+      date.getFullYear(),
+    ].join('/')
+  }
+
+
+  function monthValueHandler(event: any) {
+    setFinancialMonth(event.target.value)
+  }
+  function yearValueHandler(event: any) {
+    setFinancialYear(event.target.value)
+  }
+
+
+  let datasRevenues = daysInMonth?.map((dates: any) => dataFinancial?.financial.map((financial: any) => dates?.toISOString().split('T')[0] === financial.date.split('T')[0] && financial.revenuesExpenses === true ? financial.value : 0).reduce((acc: any, item: any) => acc + item))
+
+  let datasExpenses = daysInMonth?.map((dates: any) => dataFinancial?.financial.map((financial: any) => dates?.toISOString().split('T')[0] === financial.date.split('T')[0] && financial.revenuesExpenses === false ? financial.value : 0).reduce((acc: any, item: any) => acc + item))
+
+  let datasRevenuesPending = daysInMonth?.map((dates: any) => dataFinancial?.financial.map((financial: any) => dates?.toISOString().split('T')[0] === financial.date.split('T')[0] && financial.revenuesExpenses === true && financial.paidOut === 'Pendente' ? financial.value : 0).reduce((acc: any, item: any) => acc + item))
+
+  let datasExpensesPending = daysInMonth?.map((dates: any) => dataFinancial?.financial.map((financial: any) => dates?.toISOString().split('T')[0] === financial.date.split('T')[0] && financial.revenuesExpenses === false && financial.paidOut === 'Pendente' ? financial.value : 0).reduce((acc: any, item: any) => acc + item))
+
+
+  const LineData = {
+    labels: daysInMonth?.map((dates: any) => formatDate(dates)),
+    datasets: [
+      {
+        fill: true,
+        label: 'Receitas',
+        data: datasRevenues,
+        borderColor: 'rgb(65, 176, 37)',
+        backgroundColor: 'rgba(32, 248, 3, 0.5)',
+      },
+      {
+        fill: true,
+        label: 'Despesas',
+        data: datasExpenses,
+        borderColor: 'rgb(255, 15, 15)',
+        backgroundColor: 'rgba(255, 0, 0, 0.5)',
+      },
+      {
+        fill: true,
+        label: 'A receber',
+        data: datasRevenuesPending,
+        borderColor: 'rgb(53, 174, 235)',
+        backgroundColor: 'rgba(53, 181, 235, 0.5)',
+      },
+      {
+        fill: true,
+        label: 'A pagar',
+        data: datasExpensesPending,
+        borderColor: 'rgb(235, 93, 53)',
+        backgroundColor: 'rgba(235, 99, 53, 0.5)',
+      },
+    ],
+  };
 
   return (
     <Container>
       <h3>Registros de transações</h3>
       <Content>
         <Flex2>
-          <Input type='date' />
+          <select title='year' onChange={(event) => yearValueHandler(event)}>
+            <option value={year} >{year}</option>
+            <option value={year - 1} >{year - 1}</option>
+            <option value={year - 2} >{year - 2}</option>
+            <option value={year - 3} >{year - 3}</option>
+            <option value={year - 4} >{year - 4}</option>
+            <option value={year - 5} >{year - 5}</option>
+            <option value={year - 6} >{year - 6}</option>
+            <option value={year - 7} >{year - 7}</option>
+            <option value={year - 8} >{year - 8}</option>
+            <option value={year - 9} >{year - 9}</option>
+            <option value={year - 10} >{year - 10}</option>
+          </select>
+          <select title='month' value={financialMonth} onChange={(event) => monthValueHandler(event)}>
+            <option value='1' >Janeiro</option>
+            <option value='2' >Fevereiro</option>
+            <option value='3' >Março</option>
+            <option value='4' >Abril</option>
+            <option value='5' >Maio</option>
+            <option value='6' >Junho</option>
+            <option value='7' >Julho</option>
+            <option value='8' >Agosto</option>
+            <option value='9' >Setembro</option>
+            <option value='10' >Outubro</option>
+            <option value='11' >Novembro</option>
+            <option value='12' >Dezembro</option>
+          </select>
           <Input type='text' placeholder='Tipo' />
           <Input type='text' placeholder='status' />
           <Input type='text' placeholder='Contas' />
@@ -119,12 +170,12 @@ const FinancialTransactions = () => {
             </AsideHeader>
             <AsideContent>
               <p><b>Total recebido:</b></p>
-              <p>R$ 0,00</p>
+              <p>{formatter.format(datasRevenues.reduce((acc: number, item: number) => acc + item))}</p>
               <p><b>Total pago:</b></p>
-              <span>R$ -0,00</span>
+              <span>-{formatter.format(datasExpenses.reduce((acc: number, item: number) => acc + item))}</span>
               <hr />
-              <p><b>A receber:</b></p><p>R$ 0,00</p>
-              <p><b>A pagar:</b></p><span>R$ -0,00</span>
+              <p><b>A receber:</b></p><p>{formatter.format(datasRevenuesPending.reduce((acc: number, item: number) => acc + item))}</p>
+              <p><b>A pagar:</b></p><span>-{formatter.format(datasExpensesPending.reduce((acc: number, item: number) => acc + item))}</span>
               <hr />
               <Button>Mais relatórios</Button>
             </AsideContent>
@@ -160,12 +211,13 @@ const FinancialTransactions = () => {
                 </Thead>
                 <Tbody>
                   {dataFinancial?.financial.map((financial: any, index: number) => {
-                    return (
+                    return new Date(financial.date.split('T'[0])).getMonth() + 1 === parseInt(financialMonth) && new Date(financial.date.split('T'[0])).getFullYear() === parseInt(financialYear) ?
+
                       <Tr key={index}>
                         <Td><Input type='checkbox' /></Td>
                         <Td>{financial.date.split('T')[0]}</Td>
                         <Td>{financial.description}</Td>
-                        <Td>{formatter.format(financial.value)}</Td>
+                        <Td className={financial.revenuesExpenses ? 'green' : 'red'}>{formatter.format(financial.value)}</Td>
                         <Td>{financial.receivedFrom}</Td>
                         <Td>{financial.category}</Td>
                         <Td>{financial.account}</Td>
@@ -187,13 +239,14 @@ const FinancialTransactions = () => {
                               financial.notes,
                               financial.file,
                               financial.frequenci,
-                              financial.repetition)}><BsFillPencilFill /></Button>
+                              financial.repetition,
+                              financial.revenuesExpenses)}><BsFillPencilFill /></Button>
                             <Button><AiFillCopy /></Button>
                             <Button type='button' onClick={() => handleDelete(financial._id)}><BsFillTrashFill /></Button>
                           </ButtonContainer>
                         </Td>
                       </Tr>
-                    )
+                      : null
                   })}
                 </Tbody>
               </Table>
