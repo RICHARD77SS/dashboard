1c6467e53f554c3f13755708276123c044f51ff9
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-11-30 21:42:26 -0300

    Dados dos cards e filtro dos botões

--- src/Components/Financial/index.tsx
+++ src/Components/Financial/index.tsx
@@ -23,33 +23,118 @@ import { formatter } from '../../utils/formatMoneyBr';
 const Financial = () => {
   const { OpenRevenues, OpenExpenses } = React.useContext(FinancialContext)
   const { data: dataFinancial } = useAxios('financial')
+
+
   let dateNow = new Date()
   var day = dateNow.getDate()
   var month = dateNow.getMonth() + 1
   var year = dateNow.getFullYear()
 
+  const [controlls1Day, setControlls1Day] = React.useState(day)
+  const [controlls1Month, setControlls1Month] = React.useState(month)
+
+  const [controlls2Day, setControlls2Day] = React.useState(day)
+  const [controlls2Month, setControlls2Month] = React.useState(month)
+
+  const [controlls3Day, setControlls3Day] = React.useState(day)
+  const [controlls3Month, setControlls3Month] = React.useState(month)
+
+  const [controlls4Day, setControlls4Day] = React.useState(day)
+  const [controlls4Month, setControlls4Month] = React.useState(month)
+
+  const [controlls5Day, setControlls5Day] = React.useState(day)
+  const [controlls5Month, setControlls5Month] = React.useState(month)
+
+  const [controlls6Day, setControlls6Day] = React.useState(day)
+  const [controlls6Month, setControlls6Month] = React.useState(month)
+
   let days = dataFinancial?.financial.map((financial: any) => {
     return { "data": new Date(financial.date.split('T')[0]), "valor": financial.value, "despesas": financial.revenuesExpenses, "status": financial.paidOut }
   })
 
-  let PendingExpenses = days?.map((dias: any) => dias.data.getMonth() + 1 === month && dias.data.getDate() + 1 < day && dias.status === 'Pendente' && dias.despesas === false ? dias.valor : 0)
-  let AllPendingExpenses = PendingExpenses?.reduce((acc: any, item: any) => acc + item)
 
-  let NowPendingExpenses = days?.map((dias: any) => dias.data.getMonth() + 1 === month && dias.data.getDate() + 1 === day && dias.status === 'Pendente' && dias.despesas === false ? dias.valor : 0)
-  let AllNowPendingExpenses = NowPendingExpenses?.reduce((acc: any, item: any) => acc + item)
+  let receivedToday = days?.map((dias: any) => dias.data.getMonth() + 1 === controlls1Month && dias.data.getDate() + 1 === controlls1Day && dias.status === 'Pago' && dias.despesas === true ? dias.valor : 0)
+  let AllReceivedToday = receivedToday?.reduce((acc: any, item: any) => acc + item)
 
-  let NowExpenses = days?.map((dias: any) => dias.data.getMonth() + 1 === month && dias.data.getDate() + 1 === day && dias.status === 'Pago' && dias.despesas === false ? dias.valor : 0)
-  let AllNowExpenses = NowExpenses?.reduce((acc: any, item: any) => acc + item)
+  let paidToday = days?.map((dias: any) => dias.data.getMonth() + 1 === controlls2Month && dias.data.getDate() + 1 === controlls2Day && dias.status === 'Pago' && dias.despesas === false ? dias.valor : 0)
+  let AllPaidToday = paidToday?.reduce((acc: any, item: any) => acc + item)
+
+  let toReceiveToday = days?.map((dias: any) => dias.data.getMonth() + 1 === controlls3Month && dias.data.getDate() + 1 === controlls3Day && dias.status === 'Pendente' && dias.despesas === true ? dias.valor : 0)
+  let AllToReceiveToday = toReceiveToday?.reduce((acc: any, item: any) => acc + item)
+
+  let toPayToday = days?.map((dias: any) => dias.data.getMonth() + 1 === controlls4Month && dias.data.getDate() + 1 === controlls4Day && dias.status === 'Pendente' && dias.despesas === false ? dias.valor : 0)
+  let AlltoPayToday = toPayToday?.reduce((acc: any, item: any) => acc + item)
+
+  let overdueReceipt = days?.map((dias: any) => dias.data.getMonth() + 1 === controlls5Month && dias.data.getDate() + 1 < controlls5Day && dias.status === 'Pendente' && dias.despesas === true ? dias.valor : 0)
+  let AllOverdueReceipt = overdueReceipt?.reduce((acc: any, item: any) => acc + item)
+
+  let latePayment = days?.map((dias: any) => dias.data.getMonth() + 1 === controlls6Month && dias.data.getDate() + 1 < controlls6Day && dias.status === 'Pendente' && dias.despesas === false ? dias.valor : 0)
+  let AllLatePayment = latePayment?.reduce((acc: any, item: any) => acc + item)
 
 
-  let PendingRevenues = days?.map((dias: any) => dias.data.getMonth() + 1 === month && dias.data.getDate() + 1 < day && dias.status === 'Pendente' && dias.despesas === true ? dias.valor : 0)
-  let AllPendingRevenues = PendingRevenues?.reduce((acc: any, item: any) => acc + item)
 
-  let NowPendingRevenues = days?.map((dias: any) => dias.data.getMonth() + 1 === month && dias.data.getDate() + 1 === day && dias.status === 'Pendente' && dias.despesas === true ? dias.valor : 0)
-  let AllNowPendingRevenues = NowPendingRevenues?.reduce((acc: any, item: any) => acc + item)
+  function controlls1DayHandler(date: any) {
+    setControlls1Day(date)
+    setControlls1Month(month)
+  }
+  function controlls1MonthHandler(date: any) {
+    setControlls1Month(date)
+    setControlls1Day(day)
+  }
 
-  let NowRevenues = days?.map((dias: any) => dias.data.getMonth() + 1 === month && dias.data.getDate() + 1 === day && dias.status === 'Pago' && dias.despesas === true ? dias.valor : 0)
-  let AllNowRevenues = NowRevenues?.reduce((acc: any, item: any) => acc + item)
+
+  function controlls2DayHandler(date: any) {
+    setControlls2Day(date)
+    setControlls2Month(month)
+  }
+  function controlls2MonthHandler(date: any) {
+    setControlls2Month(date)
+    setControlls2Day(day)
+  }
+
+
+  function controlls3DayHandler(date: any) {
+    setControlls3Day(date)
+    setControlls3Month(month)
+  }
+  function controlls3MonthHandler(date: any) {
+    setControlls3Month(date)
+    setControlls3Day(day)
+  }
+
+
+  function controlls4DayHandler(date: any) {
+    setControlls4Day(date)
+    setControlls4Month(month)
+  }
+  function controlls4MonthHandler(date: any) {
+    setControlls4Month(date)
+    setControlls4Day(day)
+  }
+
+
+  function controlls5DayHandler(date: any) {
+    setControlls5Day(date)
+    setControlls5Month(month)
+  }
+  function controlls5MonthHandler(date: any) {
+    setControlls5Month(date)
+    setControlls5Day(day)
+  }
+
+
+  function controlls6DayHandler(date: any) {
+    setControlls6Day(date)
+    setControlls6Month(month)
+  }
+  function controlls6MonthHandler(date: any) {
+    setControlls6Month(date)
+    setControlls6Day(day)
+  }
+
+
+
+
   console.log()
   return (
     <Container>
@@ -61,13 +146,13 @@ const Financial = () => {
               <h3><GiConfirmed /> Recebido hoje</h3>
             </HeaderMiniBox>
             <ContentMiniBox>
-              <h3> {formatter.format(AllNowRevenues)}</h3>
-              <p>Total do mes:  {formatter.format(AllNowRevenues)}</p>
+              <h3> {formatter.format(AllReceivedToday)}</h3>
+              <p>Total do mes:  {formatter.format(AllReceivedToday)}</p>
               <ButtonContainer>
-                <Button><p>Hoje</p></Button>
-                <Button><p>+7 dias</p></Button>
-                <Button><p>+1 mês</p></Button>
-                <Button><p>+3 meses</p></Button>
+                <Button type='button' onClick={() => controlls1DayHandler(day)}><p>Hoje</p></Button>
+                <Button type='button' onClick={() => controlls1DayHandler(day + 7)}><p>+7 dias</p></Button>
+                <Button type='button' onClick={() => controlls1MonthHandler(month + 2)}><p>+1 mês</p></Button>
+                <Button type='button' onClick={() => controlls1MonthHandler(month + 4)}><p>+3 meses</p></Button>
               </ButtonContainer>
             </ContentMiniBox>
           </MiniBox>
@@ -76,13 +161,13 @@ const Financial = () => {
               <h3><GiConfirmed /> Pago hoje</h3>
             </HeaderMiniBox>
             <ContentMiniBox>
-              <h3> {formatter.format(AllNowExpenses)}</h3>
-              <p>Total do mes:  {formatter.format(AllNowExpenses)}</p>
+              <h3> {formatter.format(AllPaidToday)}</h3>
+              <p>Total do mes:  {formatter.format(AllPaidToday)}</p>
               <ButtonContainer>
-                <Button><p>Hoje</p></Button>
-                <Button><p>+7 dias</p></Button>
-                <Button><p>+1 mês</p></Button>
-                <Button><p>+3 meses</p></Button>
+                <Button type='button' onClick={() => controlls2DayHandler(day)}><p>Hoje</p></Button>
+                <Button type='button' onClick={() => controlls2DayHandler(day + 7)}><p>+7 dias</p></Button>
+                <Button type='button' onClick={() => controlls2MonthHandler(month + 2)}><p>+1 mês</p></Button>
+                <Button type='button' onClick={() => controlls2MonthHandler(month + 4)}><p>+3 meses</p></Button>
               </ButtonContainer>
             </ContentMiniBox>
           </MiniBox>
@@ -92,13 +177,13 @@ const Financial = () => {
               <h3><FiAlertOctagon /> A receber hoje</h3>
             </HeaderMiniBox>
             <ContentMiniBox>
-              <h3> {formatter.format(AllNowPendingRevenues)}</h3>
-              <p>Total do mes:  </p>
+              <h3> {formatter.format(AllToReceiveToday)}</h3>
+              <p>Total do mes:  {formatter.format(AllToReceiveToday)}</p>
               <ButtonContainer>
-                <Button><p>Hoje</p></Button>
-                <Button><p>+7 dias</p></Button>
-                <Button><p>+1 mês</p></Button>
-                <Button><p>+3 meses</p></Button>
+                <Button type='button' onClick={() => controlls3DayHandler(day)}><p>Hoje</p></Button>
+                <Button type='button' onClick={() => controlls3DayHandler(day + 7)}><p>+7 dias</p></Button>
+                <Button type='button' onClick={() => controlls3MonthHandler(month + 2)}><p>+1 mês</p></Button>
+                <Button type='button' onClick={() => controlls3MonthHandler(month + 4)}><p>+3 meses</p></Button>
               </ButtonContainer>
             </ContentMiniBox>
           </MiniBox>
@@ -107,13 +192,13 @@ const Financial = () => {
               <h3><FiAlertOctagon /> A pagar hoje</h3>
             </HeaderMiniBox>
             <ContentMiniBox>
-              <h3> {formatter.format(AllNowPendingExpenses)}</h3>
-              <p>Total do mes:  {formatter.format(AllNowPendingExpenses)}</p>
+              <h3> {formatter.format(AlltoPayToday)}</h3>
+              <p>Total do mes:  {formatter.format(AlltoPayToday)}</p>
               <ButtonContainer>
-                <Button><p>Hoje</p></Button>
-                <Button><p>+7 dias</p></Button>
-                <Button><p>+1 mês</p></Button>
-                <Button><p>+3 meses</p></Button>
+                <Button type='button' onClick={() => controlls4DayHandler(day)}><p>Hoje</p></Button>
+                <Button type='button' onClick={() => controlls4DayHandler(day + 7)}><p>+7 dias</p></Button>
+                <Button type='button' onClick={() => controlls4MonthHandler(month + 2)}><p>+1 mês</p></Button>
+                <Button type='button' onClick={() => controlls4MonthHandler(month + 4)}><p>+3 meses</p></Button>
               </ButtonContainer>
             </ContentMiniBox>
           </MiniBox>
@@ -123,13 +208,13 @@ const Financial = () => {
               <h3><CgSandClock /> Recebimento em atraso</h3>
             </HeaderMiniBox>
             <ContentMiniBox>
-              <h3> {formatter.format(AllPendingRevenues)}</h3>
-              <p>Total do mes:  {formatter.format(AllPendingRevenues)}</p>
+              <h3> {formatter.format(AllOverdueReceipt)}</h3>
+              <p>Total do mes:  {formatter.format(AllOverdueReceipt)}</p>
               <ButtonContainer>
-                <Button><p>Hoje</p></Button>
-                <Button><p>+7 dias</p></Button>
-                <Button><p>+1 mês</p></Button>
-                <Button><p>+3 meses</p></Button>
+                <Button type='button' onClick={() => controlls5DayHandler(day)}><p>Hoje</p></Button>
+                <Button type='button' onClick={() => controlls5DayHandler(day + 7)}><p>+7 dias</p></Button>
+                <Button type='button' onClick={() => controlls5MonthHandler(month + 2)}><p>+1 mês</p></Button>
+                <Button type='button' onClick={() => controlls5MonthHandler(month + 4)}><p>+3 meses</p></Button>
               </ButtonContainer>
             </ContentMiniBox>
           </MiniBox>
@@ -138,13 +223,13 @@ const Financial = () => {
               <h3><CgSandClock /> Pagamento em atraso</h3>
             </HeaderMiniBox>
             <ContentMiniBox>
-              <h3>  {formatter.format(AllPendingExpenses)}</h3>
-              <p>Total do mes:  {formatter.format(AllPendingExpenses)}</p>
+              <h3>  {formatter.format(AllLatePayment)}</h3>
+              <p>Total do mes:  {formatter.format(AllLatePayment)}</p>
               <ButtonContainer>
-                <Button><p>Hoje</p></Button>
-                <Button><p>+7 dias</p></Button>
-                <Button><p>+1 mês</p></Button>
-                <Button><p>+3 meses</p></Button>
+                <Button type='button' onClick={() => controlls6DayHandler(day)}><p>Hoje</p></Button>
+                <Button type='button' onClick={() => controlls6DayHandler(day + 7)}><p>+7 dias</p></Button>
+                <Button type='button' onClick={() => controlls6MonthHandler(month + 2)}><p>+1 mês</p></Button>
+                <Button type='button' onClick={() => controlls6MonthHandler(month + 4)}><p>+3 meses</p></Button>
               </ButtonContainer>
             </ContentMiniBox>
           </MiniBox>
@@ -188,7 +273,7 @@ const Financial = () => {
         </Block>
       </Content>
       <FinancialGraphs />
-    </Container>
+    </Container >
   )
 }
 
