ee3ba7ff64676ae589970ac3e4d5aeccb16f3a2c
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-12-03 22:55:38 -0300

    Contexto categoria de contatos

---
+++ src/contexts/contactCategoryContext.js
@@ -0,0 +1,95 @@
+import React from 'react'
+
+import api from '../services/api';
+
+import { useAxios } from '../hooks/useAxios';
+import ModalContactCategoriesEdit from '../Components/ModalContactCategoriesEdit';
+
+export const ContactCategoryContext = React.createContext();
+
+export function ContactCategoryContextProvider({ children }) {
+  const { data, mutate } = useAxios('contactcategory');
+
+  const [id, setId] = React.useState();
+  const [name, setName] = React.useState()
+  const [description, setDescription] = React.useState()
+
+  const [openModal, setOpenModal] = React.useState(false)
+
+  function nameHandler(event) {
+    setName(event.target.value);
+  }
+  function descriptionHandler(event) {
+    setDescription(event.target.value);
+  }
+
+  function CloseModal() {
+    setOpenModal(false)
+    setName('')
+    setDescription('')
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const contactCategory = {
+      name,
+      description
+    }
+    if (id) {
+      api.put(`contactcategory/${id}`, contactCategory)
+      window.alert('contactcategory Editada')
+      const updatedContactCategory = {
+        contactCategory: data.contactCategory?.map((contactCategory) => {
+          if (contactCategory._id === id) {
+            return {
+              ...contactCategory,
+              name,
+              description
+            };
+          }
+          return contactCategory
+        }),
+      };
+      mutate(updatedContactCategory, false)
+    } else {
+      api.post('contactcategory', contactCategory);
+      window.alert('contactcategory adicionada')
+      const updatedContactCategory = {
+        contactCategory: [...data.contactCategory, contactCategory]
+      }
+      mutate(updatedContactCategory, false)
+    }
+  }
+  function handleDelete(id) {
+    api.delete(`contactcategory/${id}`);
+    window.alert('contactCategory removido')
+    const updatedContactCategory = {
+      contactCategory: data.contactCategory?.filter((contactCategory) => contactCategory._id !== id)
+    };
+    mutate(updatedContactCategory, false)
+  }
+
+  function handleEdit(contactcategoryId, contactcategoryName, contactcategoryDescription) {
+    setName(contactcategoryName);
+    setDescription(contactcategoryDescription);
+    setId(contactcategoryId);
+    setOpenModal(true);
+  }
+  return <ContactCategoryContext.Provider value={{
+    name,
+    description,
+    nameHandler,
+    descriptionHandler,
+    handleSubmit,
+    handleDelete,
+    setId,
+    id,
+    openModal,
+    handleEdit,
+    setOpenModal,
+    CloseModal
+  }}>
+    {children}
+    {openModal && <ModalContactCategoriesEdit />}
+  </ContactCategoryContext.Provider>
+}
