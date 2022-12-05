7c099fd946fb1477aa076e677b22cf1f675af154
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-12-04 22:47:41 -0300

    Modal para editar contas

---
+++ src/Components/ModalAccountEdit/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Input from '../Input';
+
+import { Container, Content, Form } from './styles'
+
+import { AccountContext } from '../../contexts/accountContext';
+import Button from '../Button';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+
+const ModalAccountEdit = () => {
+  const { name, description, nameHandler, descriptionHandler, handleSubmit, CloseModal } = React.useContext(AccountContext)
+  return (
+    <Container>
+      <Content>
+        <Form onSubmit={handleSubmit}>
+          <Button type='button' onClick={() => CloseModal()}><AiOutlineCloseCircle size='30' /></Button>
+          <Input type='text' value={name} onChange={nameHandler} />
+          <Input type='text' value={description} onChange={descriptionHandler} />
+          <Button type='submit'>Editar</Button>
+        </Form>
+      </Content>
+    </Container>
+  )
+}
+
+export default ModalAccountEdit;
