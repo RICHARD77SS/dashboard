664292f90d192edaa30ad4b835f2f6922df6bd68
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-12-10 16:27:55 -0300

    Dados inseridos na tabela e grafico

--- src/Components/FinancialReportsExtract/index.tsx
+++ src/Components/FinancialReportsExtract/index.tsx
@@ -15,76 +15,140 @@ import Th from '../Th';
 import Thead from '../Thead';
 import TopTableOptions from '../TopTableOptions';
 import Tr from '../Tr';
+import React from 'react'
+import { useAxios } from '../../hooks/useAxios';
+import { formatter } from '../../utils/formatMoneyBr';
+import InputBlock from '../InputBlock';
 
-const LineData = {
-  labels: [
-    '01/01/2022',
-    '02/01/2022',
-    '03/01/2022',
-    '04/01/2022',
-    '05/01/2022',
-    '06/01/2022',
-    '07/01/2022',
-    '08/01/2022',
-    '09/01/2022',
-    '10/01/2022',
-    '11/01/2022',
-    '12/01/2022',
-    '13/01/2022',
-    '14/01/2022',
-    '15/01/2022',
-    '16/01/2022',
-    '17/01/2022',
-    '18/01/2022',
-    '19/01/2022',
-    '20/01/2022',
-    '21/01/2022',
-    '22/01/2022',
-    '23/01/2022',
-    '24/01/2022',
-    '25/01/2022',
-    '26/01/2022',
-    '27/01/2022',
-    '28/01/2022',
-    '29/01/2022',
-    '30/01/2022'
-  ],
-  datasets: [
-    {
-      fill: true,
-      label: 'Receitas',
-      data: [222, 212, 313, 414, 166, 778, 119, 0, 100, 200, 400, 500, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100],
-      borderColor: 'rgb(65, 176, 37)',
-      backgroundColor: 'rgba(32, 248, 3, 0.5)',
-    },
-    {
-      fill: true,
-      label: 'Despesas',
-      data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      borderColor: 'rgb(255, 15, 15)',
-      backgroundColor: 'rgba(255, 0, 0, 0.5)',
-    },
-    {
-      fill: true,
-      label: 'A receber',
-      data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      borderColor: 'rgb(53, 174, 235)',
-      backgroundColor: 'rgba(53, 181, 235, 0.5)',
-    },
-    {
-      fill: true,
-      label: 'A pagar',
-      data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      borderColor: 'rgb(235, 93, 53)',
-      backgroundColor: 'rgba(235, 99, 53, 0.5)',
-    },
-  ],
-};
 const FinancialReportsExtract = () => {
+  const { data: dataFinancial } = useAxios('financial')
+  let dateNow = new Date()
+  var day = dateNow.getDate()
+  var month = dateNow.getMonth() + 1
+  var year = dateNow.getFullYear()
+  const [financialMonth, setFinancialMonth] = React.useState(month.toString())
+  const [financialYear, setFinancialYear] = React.useState(year.toString())
+
+  let date = new Date(`${financialYear}-${financialMonth}-01`)
+
+  function getAllDays(years: any, months: any) {
+    const date = new Date(years, months, 1);
+    const dates = []
+
+    while (date.getMonth() === months) {
+      dates.push(new Date(date));
+      date.setDate(date.getDate() + 1);
+    }
+    return dates
+  }
+
+  const monthDays: number[] = getAllDays(date.getFullYear(), date.getMonth()).map((date: any) => new Date(date).getDate())
+
+  let days = dataFinancial?.financial.map((financial: any) => {
+    return { "data": new Date(financial.date.split('T')[0]), "valor": financial.value, "despesas": financial.revenuesExpenses, "status": financial.paidOut }
+  })
+
+
+  let daysValuesRevenues = monthDays?.map((dayy: any) => {
+    let vals = days?.map((dateDay: any) => dateDay.data.getDate() + 1 === dayy && dateDay.data.getMonth() + 1 === parseInt(financialMonth) && dateDay.despesas === true && dateDay.status === 'Pago' ? dateDay.valor : 0).filter((dd: any) => dd !== 0)
+
+    return vals?.length === 0 ? 0 : vals?.reduce(function (soma: any, i: any) {
+      return soma + i;
+    });
+  })
+  let daysValuesExpenses = monthDays?.map((dayy: any) => {
+    let vals = days?.map((dateDay: any) => dateDay.data.getDate() + 1 === dayy && dateDay.data.getMonth() + 1 === parseInt(financialMonth) && dateDay.despesas === false && dateDay.status === 'Pago' ? dateDay.valor : 0).filter((dd: any) => dd !== 0)
+
+    return vals?.length === 0 ? 0 : vals?.reduce(function (soma: any, i: any) {
+      return soma + i;
+    });
+  })
+  let daysValuesRevenuesPending = monthDays?.map((dayy: any) => {
+    let vals = days?.map((dateDay: any) => dateDay.data.getDate() + 1 === dayy && dateDay.data.getMonth() + 1 === parseInt(financialMonth) && dateDay.despesas === true && dateDay.status === 'Pendente' ? dateDay.valor : 0).filter((dd: any) => dd !== 0)
+
+    return vals?.length === 0 ? 0 : vals?.reduce(function (soma: any, i: any) {
+      return soma + i;
+    });
+  })
+  let daysValuesExpensesPending = monthDays?.map((dayy: any) => {
+    let vals = days?.map((dateDay: any) => dateDay.data.getDate() + 1 === dayy && dateDay.data.getMonth() + 1 === parseInt(financialMonth) && dateDay.despesas === false && dateDay.status === 'Pendente' ? dateDay.valor : 0).filter((dd: any) => dd !== 0)
+
+    return vals?.length === 0 ? 0 : vals?.reduce(function (soma: any, i: any) {
+      return soma + i;
+    });
+  })
+
+
+  const LineData = {
+    labels: monthDays,
+    datasets: [
+      {
+        fill: true,
+        label: 'Receitas',
+        data: daysValuesRevenues,
+        borderColor: 'rgb(65, 176, 37)',
+        backgroundColor: 'rgba(32, 248, 3, 0.5)',
+      },
+      {
+        fill: true,
+        label: 'Despesas',
+        data: daysValuesExpenses,
+        borderColor: 'rgb(255, 15, 15)',
+        backgroundColor: 'rgba(255, 0, 0, 0.5)',
+      },
+      {
+        fill: true,
+        label: 'A receber',
+        data: daysValuesRevenuesPending,
+        borderColor: 'rgb(53, 174, 235)',
+        backgroundColor: 'rgba(53, 181, 235, 0.5)',
+      },
+      {
+        fill: true,
+        label: 'A pagar',
+        data: daysValuesExpensesPending,
+        borderColor: 'rgb(235, 93, 53)',
+        backgroundColor: 'rgba(235, 99, 53, 0.5)',
+      },
+    ],
+  };
+  function monthValueHandler(event: any) {
+    setFinancialMonth(event.target.value)
+  }
+  function yearValueHandler(event: any) {
+    setFinancialYear(event.target.value)
+  }
+  console.log(financialMonth + '-' + financialYear)
+  console.log(month.toString())
   return (
     <Container>
       <ReportsHeader logo='' corporation='Inc name' reportsName='Fluxo de caixa - Extrato' />
       <h4>Período: 01/09/2022 á 30/12/2022</h4>
+      <Flex>
+        <select title='year' onChange={(event) => yearValueHandler(event)}>
+          <option value={year} >{year}</option>
+          <option value={year - 1} >{year - 1}</option>
+          <option value={year - 2} >{year - 2}</option>
+          <option value={year - 3} >{year - 3}</option>
+          <option value={year - 4} >{year - 4}</option>
+          <option value={year - 5} >{year - 5}</option>
+        </select>
+        <select title='month' value={financialMonth} onChange={(event) => monthValueHandler(event)}>
+          <option value='1' >Janeiro</option>
+          <option value='2' >Fevereiro</option>
+          <option value='3' >Março</option>
+          <option value='4' >Abril</option>
+          <option value='5' >Maio</option>
+          <option value='6' >Junho</option>
+          <option value='7' >Julho</option>
+          <option value='8' >Agosto</option>
+          <option value='9' >Setembro</option>
+          <option value='10' >Outubro</option>
+          <option value='11' >Novembro</option>
+          <option value='12' >Dezembro</option>
+        </select>
+
+      </Flex>
       <Graph>
         <GraphLineArea data={LineData} />
       </Graph>
@@ -105,12 +169,18 @@ const FinancialReportsExtract = () => {
             </Tr>
           </Thead>
           <Tbody>
-            <Tr>
-              <Td>21/02/2020</Td>
-              <Td>descriptions</Td>
-              <Td>R$ 1.000,00</Td>
-              <Td>categories</Td>
-            </Tr>
+            {dataFinancial?.financial.map((financial: any, index: number) => {
+              let monthTable = new Date(financial.date).getMonth() + 1
+              let yearTable = new Date(financial.date).getFullYear()
+              return monthTable.toString() === financialMonth && yearTable.toString() === financialYear ?
+                <Tr key={index}>
+                  <Td>{financial.date.split('T')[0]}</Td>
+                  <Td>{financial.description}</Td>
+                  <Td>{formatter.format(financial.value)}</Td>
+                  <Td>{financial.category}</Td>
+                </Tr>
+                : null
+            })}
           </Tbody>
         </Table>
       </TableContainer>
