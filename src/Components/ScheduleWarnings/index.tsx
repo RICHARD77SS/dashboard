16471f8d9c8d4497e2bba140036692d9b2e36fad
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-12-17 21:40:31 -0300

    Exibe murais de texto e imagem com botão para editar e apagar

--- src/Components/ScheduleWarnings/index.tsx
+++ src/Components/ScheduleWarnings/index.tsx
@@ -8,7 +8,7 @@ import BoxHeader from "../BoxHeader";
 import Button from '../Button';
 import Container from "../Container";
 import Flex from '../Flex';
-import { Buttons, Mural, MuralContent } from './styles';
+import { Buttons, Mural, MuralContent, FlexContent } from './styles';
 
 const ScheduleWarnings = () => {
   const { data } = useAxios('muralstext')
@@ -18,60 +18,64 @@ const ScheduleWarnings = () => {
   return (
     <Container>
       <Box>
-        <BoxHeader title='Resultados: 0' >
+        <BoxHeader title={`Resultados: ${data?.muralsText.length + dataImage?.muralsImage.length}`} >
           <Flex>
             <Buttons onClick={() => { OpenImageModal() }}>+ Adicionar imagem</Buttons>
             <Buttons onClick={() => { OpenTextModal() }}>+ Adicionar texto</Buttons>
           </Flex>
         </BoxHeader>
         <BoxContent>
-          {data?.muralsText.map((text: any, index: number) => {
-            return (
-              <Mural key={index}>
-                <MuralContent backColor={text.backColor} textColor={text.textColor}>
-                  <h3>{text.name}</h3>
-                  <h3>{text.description}</h3>
-                </MuralContent>
-                <Flex>
-                  <Button type='button' onClick={() => {
-                    handleEdit(
-                      text._id,
-                      text.name,
-                      text.description,
-                      text.textColor,
-                      text.backColor,
-                      text.background,
-                      text.status,
-                      text.unpublish,
-                      text.link,
-                      text.notification)
-                  }}>Editar</Button>
-                  <Button type='button' onClick={() => { handleDelete(text._id) }}>Remover</Button>
-                </Flex>
-              </Mural>
-            )
-          })}
-          {dataImage?.muralsImage.map((image: any, index: number) => {
-            return (
-              <Mural key={index}>
-                <MuralContent >
-                  <img src={image.image} alt="" />
-                </MuralContent>
-                <Flex>
-                  <Button type='button' onClick={() => {
-                    handleEditImage(
-                      image._id,
-                      image.image,
-                      image.status,
-                      image.unpublish,
-                      image.link,
-                      image.notification)
-                  }}>Editar</Button>
-                  <Button type='button' onClick={() => { handleDeleteImage(image._id) }}>Remover</Button>
-                </Flex>
-              </Mural>
-            )
-          })}
+          <FlexContent>
+
+            {data?.muralsText.map((text: any, index: number) => {
+              return (
+                <Mural key={index}>
+                  <MuralContent backColor={text.backColor} textColor={text.textColor}>
+                    <h3>{text.name}</h3>
+                    <h3>{text.description}</h3>
+                  </MuralContent>
+                  <Flex>
+                    <Button type='button' onClick={() => {
+                      handleEdit(
+                        text._id,
+                        text.name,
+                        text.description,
+                        text.textColor,
+                        text.backColor,
+                        text.background,
+                        text.status,
+                        text.unpublish,
+                        text.link,
+                        text.notification)
+                    }}>Editar</Button>
+                    <Button type='button' onClick={() => { handleDelete(text._id) }}>Remover</Button>
+                  </Flex>
+                </Mural>
+              )
+            })}
+            {dataImage?.muralsImage.map((image: any, index: number) => {
+              console.log(image.image)
+              return (
+                <Mural key={index}>
+                  <MuralContent >
+                    <img src={image.image} alt="" />
+                  </MuralContent>
+                  <Flex>
+                    <Button type='button' onClick={() => {
+                      handleEditImage(
+                        image._id,
+                        image.image,
+                        image.status,
+                        image.unpublish,
+                        image.link,
+                        image.notification)
+                    }}>Editar</Button>
+                    <Button type='button' onClick={() => { handleDeleteImage(image._id) }}>Remover</Button>
+                  </Flex>
+                </Mural>
+              )
+            })}
+          </FlexContent>
         </BoxContent>
       </Box>
 
