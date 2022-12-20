76dccc0f0a59dc0691ae2184968be020bc2379ea
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-12-19 22:24:01 -0300

    Categoty anotations context

---
+++ src/contexts/categoryAnotationsContext.js
@@ -0,0 +1,95 @@
+import React from 'react'
+
+import api from '../services/api';
+
+import { useAxios } from '../hooks/useAxios';
+import CategoriesAnotationsEdit from '../Components/CategoriesAnotationsEdit';
+
+export const CategoryAnotationsContext = React.createContext();
+
+export function CategoryAnotationsContextProvider({ children }) {
+  const { data, mutate } = useAxios('categoryanotations');
+
+  const [id, setId] = React.useState();
+  const [name, setName] = React.useState()
+  const [description, setDescription] = React.useState()
+
+  const [openModal, setOpenModal] = React.useState(false)
+
+  function nameHandler(event) {
+    setName(event.target.value);
+  }
+  function descriptionHandler(event) {
+    setDescription(event.target.value);
+  }
+
+  function CloseModal() {
+    setOpenModal(false)
+    setName('')
+    setDescription('')
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const categoryAnotations = {
+      name,
+      description
+    }
+    if (id) {
+      api.put(`categoryanotations/${id}`, categoryAnotations)
+      window.alert('categoryAnotations Editada')
+      const updatedCategoryAnotations = {
+        categoryAnotations: data.categoryAnotations?.map((categoryAnotations) => {
+          if (categoryAnotations._id === id) {
+            return {
+              ...categoryAnotations,
+              name,
+              description
+            };
+          }
+          return categoryAnotations
+        }),
+      };
+      mutate(updatedCategoryAnotations, false)
+    } else {
+      api.post('categoryanotations', categoryAnotations);
+      window.alert('categoryAnotations adicionada')
+      const updatedCategoryAnotations = {
+        categoryAnotations: [...data.categoryAnotations, categoryAnotations]
+      }
+      mutate(updatedCategoryAnotations, false)
+    }
+  }
+  function handleDelete(id) {
+    api.delete(`categoryanotations/${id}`);
+    window.alert('CategoryAnotations removido')
+    const updatedCategoryAnotations = {
+      categoryAnotations: data.categoryAnotations?.filter((categoryAnotations) => categoryAnotations._id !== id)
+    };
+    mutate(updatedCategoryAnotations, false)
+  }
+
+  function handleEdit(categoryId, categoryName, categoryDescription) {
+    setName(categoryName);
+    setDescription(categoryDescription);
+    setId(categoryId);
+    setOpenModal(true);
+  }
+  return <CategoryAnotationsContext.Provider value={{
+    name,
+    description,
+    nameHandler,
+    descriptionHandler,
+    handleSubmit,
+    handleDelete,
+    setId,
+    id,
+    openModal,
+    handleEdit,
+    setOpenModal,
+    CloseModal
+  }}>
+    {children}
+    {openModal && <CategoriesAnotationsEdit />}
+  </CategoryAnotationsContext.Provider>
+}
