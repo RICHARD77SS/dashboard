2f69170ae041739681d79771d596fc02a6f593c3
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-12-20 21:36:17 -0300

    Funções para adicionar editart apagar e exibir videos

--- src/Components/MediasVideos/index.tsx
+++ src/Components/MediasVideos/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Aside from "../Aside";
+
 import Box from "../Box";
 import BoxContent from "../BoxContent";
 import BoxHeader from "../BoxHeader";
@@ -11,50 +11,72 @@ import Input from "../Input";
 import InputBlock from "../InputBlock";
 import ModalAddVideo from '../ModalAddVideo';
 
-import { AsideContent } from './styles';
+import { AsideContent, Aside, Video, MediaVideo } from './styles';
+import { VideoAlbumContext } from '../../contexts/videoAlbumContext';
+import { useAxios } from '../../hooks/useAxios';
 
 const MediaVideos = () => {
   const [addVideo, setAddVideo] = React.useState(false)
+  const { name,
+    description,
+    media,
+    title,
+    nameHandler,
+    descriptionHandler,
+    mediaHandler,
+    titleHandler,
+    handleSubmit,
+    handleDelete,
+    handleEdit, } = React.useContext(VideoAlbumContext)
+  const { data } = useAxios('videoalbum')
   return (
     <Container>
       <h3>Álbuns de Videos</h3>
-      {addVideo ?
-        <>
-          <ModalAddVideo onClick={() => { setAddVideo(false) }}>
-            <Button onClick={() => { setAddVideo(false) }}>
-              Close
-            </Button>
-          </ModalAddVideo>
-        </> : null}
       <Content>
         <Box>
           <BoxHeader title="Resultados: 0" >
             <Flex>
-              <Button onClick={() => { setAddVideo(true) }}>Editar</Button>
-              <Button>Remover</Button>
+
             </Flex>
           </BoxHeader>
           <BoxContent>
             <Content>
-
+              {data?.videoAlbum.map((video: any, index: number) => {
+                return (
+                  <Video key={index}>
+                    <MediaVideo>
+                      <h3>{video.name}</h3>
+                    </MediaVideo>
+                    <Button type='button' onClick={() => handleDelete(video._id)}>delete</Button>
+                    <Button type='button' onClick={() => handleEdit(video._id, video.name, video.description, video.media, video.title)}>Edit</Button>
+                  </Video>
+                )
+              })}
             </Content>
           </BoxContent>
         </Box>
-        <Aside>
+        <Aside onSubmit={handleSubmit}>
           <BoxHeader title="+ Criar álbum" />
           <BoxContent>
             <AsideContent>
               <InputBlock>
-                <label htmlFor="name">Titulo</label>
-                <Input type='text' />
+                <label htmlFor="name">Titulo album</label>
+                <Input type='text' value={name} onChange={nameHandler} />
               </InputBlock>
               <InputBlock>
                 <label htmlFor="description">Descrição</label>
-                <textarea title='area' name="" id="" ></textarea>
+                <textarea title='area' name="" id="" value={description} onChange={descriptionHandler}></textarea>
               </InputBlock>
-              <Button>Criar</Button>
+              <InputBlock>
+                <label htmlFor="name">Titulo do video</label>
+                <Input type='text' value={title} onChange={titleHandler} />
+              </InputBlock>
+              <InputBlock>
+                <label htmlFor="name">Video</label>
+                <Input type='text' value={media} onChange={mediaHandler} />
+              </InputBlock>
+              <Button type='submit'>Criar</Button>
             </AsideContent>
-
           </BoxContent>
         </Aside>
       </Content>
