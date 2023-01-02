774a32fd62e823f8af0244488ef448b4758b1db1
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2023-01-01 21:53:23 -0300

    Modal para exibir controle de exibição de colunas nas tabelas

--- src/Components/TopTableOptions/index.tsx
+++ src/Components/TopTableOptions/index.tsx
@@ -1,26 +1,32 @@
 import Button from '../Button';
-import { Flex,Results,Options} from './styles';
+import { Flex, Results, Options } from './styles';
 import { AiOutlineCopy, AiOutlineDownload, AiOutlinePrinter, AiOutlineEye } from 'react-icons/ai';
 import Input from '../Input';
-
+import React from 'react'
+import ModalColumn from '../ModalColumn';
 interface Props {
   children?: React.ReactNode;
 }
 
-const TopTableOptions = ({children}:Props) => {
+const TopTableOptions = ({ children }: Props) => {
+  const [modal, setModal] = React.useState(false)
   return (
     <Flex>
       <Results>
         <Input id='result' list='numbers' type='number' step='20' min='20' max='400' />
         <label htmlFor='result'>Resultados por página</label>
       </Results>
-      {children}
       <Options>
         <Button><AiOutlineCopy /></Button>
         <Button><AiOutlineDownload /></Button>
         <Button><AiOutlinePrinter /></Button>
-        <Button><AiOutlineEye />Colunas</Button>
+        <Button onClick={() => { setModal(true) }}><AiOutlineEye />Colunas</Button>
       </Options>
+      {modal ?
+        <ModalColumn onClick={() => { setModal(false) }}>
+          {children}
+        </ModalColumn>
+        : null}
     </Flex>
   )
 }
