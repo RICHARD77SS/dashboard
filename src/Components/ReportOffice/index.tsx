6f0b35be9e18d4b1df87a7ed7c2fbea78109c269
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2023-01-05 20:54:50 -0300

    Ebxibição do nome dos usuarios com cada cargo

--- src/Components/ReportOffice/index.tsx
+++ src/Components/ReportOffice/index.tsx
@@ -1,20 +1,18 @@
 import Button from '../Button';
-import { Container, Content } from './styles';
-import { BsArrowUp, BsArrowDown } from 'react-icons/bs';
 import ReportsHeader from '../ReportsHeader';
-import { useAxios } from '../../hooks/useAxios';
 import Table from '../Table';
 import Thead from '../Thead';
 import Tr from '../Tr';
 import Th from '../Th';
 import Tbody from '../Tbody';
 import Td from '../Td';
+import { BsArrowUp, BsArrowDown } from 'react-icons/bs';
+import { Container, Content } from './styles';
+import { useAxios } from '../../hooks/useAxios';
 
 const ReportsOffice = () => {
   const { data } = useAxios('positions')
   const { data: dataPerson } = useAxios('person')
-  let offices = dataPerson?.person.map((person: any) => person.office)
-  console.log(dataPerson?.person.map((person: any) => person.office))
   return (
     <Container>
       <ReportsHeader logo='' corporation='Inc name' reportsName='Cargos' />
@@ -22,18 +20,17 @@ const ReportsOffice = () => {
         <Table>
           <Thead>
             <Tr>
-              <Th><Button>Nome completo <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
-              <Th><Button>E-mail <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+              <Th><Button>Nome <p><BsArrowUp /><BsArrowDown /></p></Button></Th>
+              <Th><Button>Pessoas<p><BsArrowUp /><BsArrowDown /></p></Button></Th>
             </Tr>
           </Thead>
           <Tbody>
             {data?.positions.map((positions: any, index: number) => {
               return (
-                <Tr>
+                <Tr key={index}>
                   <Td>{positions.name}</Td>
-                  <Td>{offices?.filter((i: string) => i === positions.name).length}</Td>
+                  <Td>{dataPerson?.person.map((person: any) => person.office.includes(positions.name) ? person.name + '; ' : null)}</Td>
                 </Tr>
-
               )
             })}
           </Tbody>
