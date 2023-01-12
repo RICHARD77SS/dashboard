a0773196ada05e67e76c771b4600e4ae80531497
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2023-01-11 22:21:57 -0300

    Ajustes para criar o cartão

--- src/Components/CardBack/index.tsx
+++ src/Components/CardBack/index.tsx
@@ -1,93 +1,87 @@
-
+import React from 'react'
 import InputBlock from "../InputBlock"
 
 import { CardBacks, Options, Flex } from "./styles"
+import { PersonalCardContext } from './../../contexts/personalCardContext';
 
 interface Props {
   cardBg?: string;
+  bgColor?: string;
+  titleColor?: string;
+  textColor?: string;
+  spanColor?: string;
 }
 
-function onDragStart(event: any) {
-  event.dataTransfer.setData('text/plain', event.target.id)
-  event.dataTransfer.setData('left', event.clientX)
-  event.dataTransfer.setData('top', event.clientY)
-  event.currentTarget.style.background = 'yellow'
-}
-function onDragEnd(event: any) {
-  event.currentTarget.style.background = 'transparent'
-}
-function onDragOver(event: any) {
-  event.preventDefault();
-}
+const CardBack = ({ cardBg, bgColor, titleColor, textColor, spanColor }: Props) => {
+  const {
+    backOnDragStart,
+    backOnDragEnd,
+    backOnDragOver,
+    backOnDrop,
+    backOnDragEnter,
+    backOnDragLeave,
+  } = React.useContext(PersonalCardContext)
 
-function onDrop(event: any) {
-  event.preventDefault()
-  var width = window.innerWidth;
-  const id = event.dataTransfer.getData('text');
-  const draggableElement = document.getElementById(id)
-  const rect = document.getElementById(id)?.getBoundingClientRect()
-
-  let left = event.dataTransfer.getData('left') - rect!.left
-  let top = event.dataTransfer.getData('top') - rect!.top
-
-  draggableElement!.style.left = `${(event.clientX - left) / width * 100}%`
-  draggableElement!.style.top = `${event.clientY - top}px`
-
-  const dropzone = event.target;
-  dropzone.appendChild(draggableElement);
-  event.dataTransfer.clearData();
-}
-const CardBack = ({ cardBg }: Props) => {
   return (
     <Flex>
-      <CardBacks cardBg={cardBg} className='drop-zone' onDragOver={(event) => onDragOver(event)} onDrop={(event) => onDrop(event)}>
-
+      <CardBacks cardBg={cardBg}
+        bgColor={bgColor}
+        titleColor={titleColor}
+        textColor={textColor}
+        spanColor={spanColor}
+        id='drop-zone'
+        className='drop-zone'
+        onDragOver={(event) => backOnDragOver(event)}
+        onDragEnter={(event) => backOnDragEnter(event)}
+        onDragLeave={(event) => backOnDragLeave(event)}
+        onDrop={(event) => backOnDrop(event)}
+      >
       </CardBacks>
       <Options>
         <img src="" alt=""
           id='dragabble-1'
           draggable='true'
-          onDragStart={(event) => onDragStart(event)}
-          onDragEnd={(event) => onDragEnd(event)}
+          onDragStart={(event) => backOnDragStart(event)}
+          onDragEnd={(event) => backOnDragEnd(event)}
         />
         <InputBlock>
           <img src="" alt=""
             id='dragabble-2'
             draggable='true'
-            onDragStart={(event) => onDragStart(event)}
-            onDragEnd={(event) => onDragEnd(event)}
+            onDragStart={(event) => backOnDragStart(event)}
+            onDragEnd={(event) => backOnDragEnd(event)}
           />
           <p
             id='dragabble-3'
             draggable='true'
-            onDragStart={(event) => onDragStart(event)}
-            onDragEnd={(event) => onDragEnd(event)}
+            onDragStart={(event) => backOnDragStart(event)}
+            onDragEnd={(event) => backOnDragEnd(event)}
           >12384</p>
         </InputBlock>
         <h3
           id='dragabble-4'
           draggable='true'
-          onDragStart={(event) => onDragStart(event)}
-          onDragEnd={(event) => onDragEnd(event)}
+          onDragStart={(event) => backOnDragStart(event)}
+          onDragEnd={(event) => backOnDragEnd(event)}
         >title</h3>
         <span
           id='dragabble-5'
           draggable='true'
-          onDragStart={(event) => onDragStart(event)}
-          onDragEnd={(event) => onDragEnd(event)}
+          onDragStart={(event) => backOnDragStart(event)}
+          onDragEnd={(event) => backOnDragEnd(event)}
         >Local</span>
         <h4
           id='dragabble-6'
           draggable='true'
-          onDragStart={(event) => onDragStart(event)}
-          onDragEnd={(event) => onDragEnd(event)}
+          onDragStart={(event) => backOnDragStart(event)}
+          onDragEnd={(event) => backOnDragEnd(event)}
         >Cartão de membro</h4>
         <InputBlock>
           <h5
             id='dragabble-7'
             draggable='true'
-            onDragStart={(event) => onDragStart(event)}
-            onDragEnd={(event) => onDragEnd(event)}
+            onDragStart={(event) => backOnDragStart(event)}
+            onDragEnd={(event) => backOnDragEnd(event)}
           >Nome completo</h5>
           <p draggable>Nome here</p>
         </InputBlock>
@@ -95,42 +89,42 @@ const CardBack = ({ cardBg }: Props) => {
           <h5
             id='dragabble-8'
             draggable='true'
-            onDragStart={(event) => onDragStart(event)}
-            onDragEnd={(event) => onDragEnd(event)}
+            onDragStart={(event) => backOnDragStart(event)}
+            onDragEnd={(event) => backOnDragEnd(event)}
           >Cargos</h5>
           <p
             id='dragabble-9'
             draggable='true'
-            onDragStart={(event) => onDragStart(event)}
-            onDragEnd={(event) => onDragEnd(event)}
+            onDragStart={(event) => backOnDragStart(event)}
+            onDragEnd={(event) => backOnDragEnd(event)}
           >Diretor</p>
         </InputBlock>
         <InputBlock>
           <h5
             id='dragabble-10'
             draggable='true'
-            onDragStart={(event) => onDragStart(event)}
-            onDragEnd={(event) => onDragEnd(event)}
+            onDragStart={(event) => backOnDragStart(event)}
+            onDragEnd={(event) => backOnDragEnd(event)}
           >Estado civil</h5>
           <p
             id='dragabble-11'
             draggable='true'
-            onDragStart={(event) => onDragStart(event)}
-            onDragEnd={(event) => onDragEnd(event)}
+            onDragStart={(event) => backOnDragStart(event)}
+            onDragEnd={(event) => backOnDragEnd(event)}
           >Casado(a)</p>
         </InputBlock>
         <InputBlock>
           <h5
             id='dragabble-12'
             draggable='true'
-            onDragStart={(event) => onDragStart(event)}
-            onDragEnd={(event) => onDragEnd(event)}
+            onDragStart={(event) => backOnDragStart(event)}
+            onDragEnd={(event) => backOnDragEnd(event)}
           >Data de nascimento</h5>
           <p
             id='dragabble-13'
             draggable='true'
-            onDragStart={(event) => onDragStart(event)}
-            onDragEnd={(event) => onDragEnd(event)}
+            onDragStart={(event) => backOnDragStart(event)}
+            onDragEnd={(event) => backOnDragEnd(event)}
           >30/08/2000</p>
         </InputBlock>
       </Options>
