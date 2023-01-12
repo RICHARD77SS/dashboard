6ee907bc9765dd2cf1600bfe69111db999ca40e9
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2023-01-11 22:23:56 -0300

    Parte da frente do cartão

--- src/Components/CardFront/index.tsx
+++ src/Components/CardFront/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import InputBlock from "../InputBlock"
 
 import { CardFronts, Options, Flex } from "./styles"
+import { PersonalCardContext } from '../../contexts/personalCardContext';
 
 interface Props {
   cardBg?: string;
@@ -10,131 +11,15 @@ interface Props {
   textColor?: string;
   spanColor?: string;
 }
-
 const CardFront = ({ cardBg, bgColor, titleColor, textColor, spanColor }: Props) => {
-
-  const [elementos, setElementos] = React.useState<any[]>([
-    {
-      id: 'draggable-1',
-      x: '25.893462059620596%',
-      y: '473.3828125px'
-    },
-    {
-      id: 'draggable-2',
-      x: '25.38039182475158%',
-      y: '278.4140625px'
-    },
-    {
-      id: 'draggable-3',
-      x: '26.799627371273715%',
-      y: '574.453125px'
-    },
-    {
-      id: 'draggable-4',
-      x: '46.85382791327913%',
-      y: '272.453125px'
-    },
-    {
-      id: 'draggable-5',
-      x: '47.16576332429991%',
-      y: '311.4375px'
-    },
-    {
-      id: 'draggable-6',
-      x: '44.50513775971093%',
-      y: '344.453125px'
-    },
-    {
-      id: 'draggable-7',
-      x: '37.72724706413731%',
-      y: '430.453125px'
-    },
-    {
-      id: 'draggable-8',
-      x: '37.72724706413731%',
-      y: '470.9375px'
-    },
-    {
-      id: 'draggable-9',
-      x: '37.72724706413731%',
-      y: '500.9453125px'
-    },
-    {
-      id: 'draggable-10',
-      x: '37.72724706413731%',
-      y: '530.9453125px'
-    },
-    {
-      id: 'draggable-11',
-      x: '37.72724706413731%',
-      y: '560.9453125px'
-    },
-    {
-      id: 'draggable-12',
-      x: '37.72724706413731%',
-      y: '600.9453125px'
-    },
-    {
-      id: 'draggable-13',
-      x: '56.17589769647696%',
-      y: '564.9296875px'
-    },
-    {
-      id: 'draggable-14',
-      x: '56.60992547425474%',
-      y: '592.9453125px'
-    },
-  ])
-
-  function onDragStart(event: any) {
-    event.dataTransfer.setData('text/plain', event.target.id)
-    event.dataTransfer.setData('left', event.clientX)
-    event.dataTransfer.setData('top', event.clientY)
-    event.currentTarget.style.background = 'yellow'
-  }
-  function onDragEnd(event: any) {
-    event.currentTarget.style.background = 'transparent'
-  }
-  function onDragOver(event: any) {
-    event.preventDefault();
-  }
-
-  function onDrop(event: any) {
-    event.preventDefault()
-    var width = window.innerWidth;
-    const id = event.dataTransfer.getData('text');
-    const draggableElement = document.getElementById(id)
-    const rect = document.getElementById(id)?.getBoundingClientRect()
-
-    let left = event.dataTransfer.getData('left') - rect!.left
-    let top = event.dataTransfer.getData('top') - rect!.top
-
-    draggableElement!.style.left = `${(event.clientX - left) / width * 100}%`
-    draggableElement!.style.top = `${event.clientY - top}px`
-
-    const dropzone = event.target;
-    dropzone.appendChild(draggableElement);
-    event.dataTransfer.clearData();
-    let data = [...elementos]
-    data[id.split('-')[1] - 1].x = `${(event.clientX - left) / width * 100}%`
-    data[id.split('-')[1] - 1].y = `${event.clientY - top}px`
-    setElementos(data)
-  }
-
-  function set(id: any, x: any, y: any) {
-    const draggableElement = document.getElementById(id)
-    draggableElement!.style.left = x
-    draggableElement!.style.top = y
-
-    const dropzone = document.getElementById('drop-zone')
-    dropzone?.appendChild(draggableElement!)
-  }
-
-  React.useEffect(() => {
-    if (elementos) {
-      elementos?.forEach((element: any) => set(element.id, element.x, element.y))
-    }
-  }, [elementos])
+  const {
+    onDragStart,
+    onDragEnd,
+    onDragOver,
+    onDrop,
+    onDragEnter,
+    onDragLeave,
+  } = React.useContext(PersonalCardContext)
 
   return (
     <Flex>
@@ -147,16 +32,19 @@ const CardFront = ({ cardBg, bgColor, titleColor, textColor, spanColor }: Props)
         id='drop-zone'
         className='drop-zone'
         onDragOver={(event) => onDragOver(event)}
-        onDrop={(event) => onDrop(event)} >
+        onDragEnter={(event) => onDragEnter(event)}
+        onDragLeave={(event) => onDragLeave(event)}
+        onDrop={(event) => onDrop(event)}
+      >
       </CardFronts>
       <Options>
-        <img src="" alt="Person"
-          id='draggable-1'
-          draggable='true'
-          onDragStart={(event) => onDragStart(event)}
-          onDragEnd={(event) => onDragEnd(event)}
-        />
         <InputBlock>
+          <img src="" alt="Person"
+            id='draggable-1'
+            draggable='true'
+            onDragStart={(event) => onDragStart(event)}
+            onDragEnd={(event) => onDragEnd(event)}
+          />
           <img src="" alt="Logo"
             id='draggable-2'
             draggable='true'
