dc92f13e31fef09dd1bd3abe90a55298a991ca2e
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2023-01-22 20:57:03 -0300

    Tipagem contexto contas

--- src/Types/@types.accountContext.ts
+++ src/Types/@types.accountContext.ts
@@ -4,7 +4,7 @@ export type AccountTypes = {
   setName: (newValue:string)=>void;
   setDescription: (newValue: string) => void;
   nameHandler: (event:React.ChangeEvent<HTMLInputElement>)=>void;
-  descriptionHandler: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  descriptionHandler: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
   handleSubmit: (event: React.ChangeEvent<HTMLFormElement>) => void;
   handleDelete: (id:string)=>void;
   setId: (newValue: string) => void;
@@ -13,4 +13,20 @@ export type AccountTypes = {
   handleEdit: (id: string,name:string,description:string) => void;
   setOpenModal: (newValue: boolean) => void;
   CloseModal: () => void;
+}
+export const initialValue = {
+  name: '',
+  description: '',
+  setName: ()=>{},
+  setDescription: () => {},
+  nameHandler: ()=>{},
+  descriptionHandler: () => {},
+  handleSubmit: () => {},
+  handleDelete: ()=>{},
+  setId: () => {},
+  id: '',
+  openModal: false,
+  handleEdit: () => {},
+  setOpenModal: () => {},
+  CloseModal: () => {},
 }
--- src/contexts/accountContext.tsx
+++ src/contexts/accountContext.tsx
@@ -5,30 +5,31 @@ import api from '../services/api';
 import { useAxios } from '../hooks/useAxios';
 import ModalAccountEdit from '../Components/ModalAccountEdit';
 import { useContext } from '../Types/@type.useContext';
+import { AccountTypes,initialValue } from '../Types/@types.accountContext';
 
-export const AccountContext = React.createContext();
+export const AccountContext = React.createContext<AccountTypes>(initialValue);
 
 export function AccountContextProvider({ children }:useContext) {
   const { data, mutate } = useAxios('account');
 
-  const [id, setId] = React.useState();
-  const [name, setName] = React.useState('')
-  const [description, setDescription] = React.useState('')
+  const [id, setId] = React.useState(initialValue.id);
+  const [name, setName] = React.useState(initialValue.name)
+  const [description, setDescription] = React.useState(initialValue.description)
 
-  const [openModal, setOpenModal] = React.useState(false)
+  const [openModal, setOpenModal] = React.useState(initialValue.openModal)
 
   function nameHandler(event:React.ChangeEvent<HTMLInputElement>) {
     setName(event.target.value);
   }
-  function descriptionHandler(event:React.ChangeEvent<HTMLInputElement>) {
+  function descriptionHandler(event:React.ChangeEvent<HTMLTextAreaElement>) {
     setDescription(event.target.value);
   }
 
   function CloseModal() {
-    setOpenModal(false)
-    setId('')
-    setName('')
-    setDescription('')
+    setOpenModal(initialValue.openModal)
+    setId(initialValue.id)
+    setName(initialValue.name)
+    setDescription(initialValue.description)
   }
 
   function handleSubmit(event:React.ChangeEvent<HTMLFormElement>) {
@@ -42,7 +43,7 @@ export function AccountContextProvider({ children }:useContext) {
       api.put(`account/${id}`, account)
       window.alert('account Editada')
       const updatedAccount = {
-        account: data.account?.map((account) => {
+        account: data.account?.map((account:{_id:string}) => {
           if (account._id === id) {
             return {
               ...account,
@@ -63,11 +64,11 @@ export function AccountContextProvider({ children }:useContext) {
       mutate(updatedAccount, false)
     }
   }
-  function handleDelete(id) {
+  function handleDelete(id:string) {
     api.delete(`account/${id}`);
     window.alert('account removido')
     const updatedAccount = {
-      account: data.account?.filter((account) => account._id !== id)
+      account: data.account?.filter((account: { _id: string }) => account._id !== id)
     };
     mutate(updatedAccount, false)
   }
@@ -81,6 +82,8 @@ export function AccountContextProvider({ children }:useContext) {
   return <AccountContext.Provider value={{
     name,
     description,
+    setName,
+    setDescription,
     nameHandler,
     descriptionHandler,
     handleSubmit,
