275b0c83d403cb473a6efed9985a97c4d5e003de
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2023-02-06 21:24:36 -0300

    ...

--- src/contexts/contactContext.tsx
+++ src/contexts/contactContext.tsx
@@ -4,10 +4,11 @@ import api from '../services/api';
 
 import { useAxios } from '../hooks/useAxios';
 import ModalContactEdit from '../Components/ModalContactEdit';
+import { useContext } from '../Types/@types.useContext';
 
 export const ContactContext = React.createContext();
 
-export function ContactContextProvider({ children }) {
+export function ContactContextProvider({ children }:useContext) {
   const { data, mutate } = useAxios('contact');
 
   const [id, setId] = React.useState();
@@ -18,28 +19,27 @@ export function ContactContextProvider({ children }) {
   const [phone1, setPhone1] = React.useState()
   const [phone2, setPhone2] = React.useState()
   const [anotations, setAnotations] = React.useState()
-
   const [openModal, setOpenModal] = React.useState(false)
 
-  function nameHandler(event) {
+  function nameHandler(event:React.ChangeEvent<HTMLInputElement>) {
     setName(event.target.value);
   }
-  function emailHandler(event) {
+  function emailHandler(event: React.ChangeEvent<HTMLInputElement>) {
     setEmail(event.target.value);
   }
-  function categoryHandler(event) {
+  function categoryHandler(event: React.ChangeEvent<HTMLSelectElement>) {
     setCategory(event.target.value);
   }
-  function typeHandler(event) {
+  function typeHandler(event: React.ChangeEvent<HTMLSelectElement>) {
     setType(event.target.value);
   }
-  function phone1Handler(event) {
+  function phone1Handler(event: React.ChangeEvent<HTMLInputElement>) {
     setPhone1(event.target.value);
   }
-  function phone2Handler(event) {
+  function phone2Handler(event: React.ChangeEvent<HTMLInputElement>) {
     setPhone2(event.target.value);
   }
-  function anotationsHandler(event) {
+  function anotationsHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
     setAnotations(event.target.value);
   }
 
@@ -55,7 +55,7 @@ export function ContactContextProvider({ children }) {
     setAnotations('')
   }
 
-  function handleSubmit(event) {
+  function handleSubmit(event: React.ChangeEvent<HTMLFormElement>) {
     event.preventDefault()
     const contact = {
       name,
@@ -70,7 +70,7 @@ export function ContactContextProvider({ children }) {
       api.put(`contact/${id}`, contact)
       window.alert('contact Editada')
       const updatedContact = {
-        contact: data.contact?.map((contact) => {
+        contact: data.contact?.map((contact:{_id:string}) => {
           if (contact._id === id) {
             return {
               ...contact,
@@ -96,22 +96,25 @@ export function ContactContextProvider({ children }) {
       mutate(updatedContact, false)
     }
   }
-  function handleDelete(id) {
+  function handleDelete(id:string) {
     api.delete(`contact/${id}`);
     window.alert('contact removido')
     const updatedContact = {
-      contact: data.contact?.filter((contact) => contact._id !== id)
+      contact: data.contact?.filter((contact: { _id: string }) => contact._id !== id)
     };
     mutate(updatedContact, false)
   }
 
-  function handleEdit(contactId, contactName,
-    contactEmail,
-    contactCategory,
-    contactType,
-    contactPhone1,
-    contactPhone2,
-    contactAnotations) {
+  function handleEdit(
+    contactId:string,
+    contactName: string,
+    contactEmail: string,
+    contactCategory:string,
+    contactType: string,
+    contactPhone1:string,
+    contactPhone2: string,
+    contactAnotations: string
+  ) {
     setId(contactId);
     setName(contactName);
     setEmail(contactEmail);
@@ -130,6 +133,13 @@ export function ContactContextProvider({ children }) {
     phone1,
     phone2,
     anotations,
+    setName,
+    setEmail,
+    setCategory,
+    setType,
+    setPhone1,
+    setPhone2,
+    setAnotations,
     nameHandler,
     emailHandler,
     categoryHandler,
